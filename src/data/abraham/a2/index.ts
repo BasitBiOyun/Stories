fee338a3324851ac3ff757e26e891a0ea37f41e1
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-08 00:29:56 +0300

    Wire Abraham A2 gold-standard data

--- src/data/abraham/a2/index.ts
+++ src/data/abraham/a2/index.ts
@@ -1,34 +1,47 @@
 import { BookData } from '../../../types';
-import { abrahamA2PagesEn } from './en/pages';
-import { abrahamA2TeacherGuideEn, abrahamA2TeacherGuideMetadataEn } from './en/teacherGuide';
-import { abrahamA2SelfStudyGuideEn, abrahamA2StudentGuideTextEn } from './en/selfStudyGuide';
-
-import { abrahamA2PagesAr } from './ar/pages';
-import { abrahamA2TeacherGuideAr, abrahamA2TeacherGuideMetadataAr } from './ar/teacherGuide';
-import { abrahamA2SelfStudyGuideAr, abrahamA2StudentGuideTextAr } from './ar/selfStudyGuide';
+import {
+  abrahamA2PagesGoldAr,
+  abrahamA2PagesGoldEn,
+  abrahamA2SelfStudyGuideGoldAr,
+  abrahamA2SelfStudyGuideGoldEn,
+  abrahamA2StudentGuideMetadataGoldAr,
+  abrahamA2StudentGuideMetadataGoldEn,
+  abrahamA2StudentGuideSectionsGoldAr,
+  abrahamA2StudentGuideSectionsGoldEn,
+  abrahamA2StudentGuideTextGoldAr,
+  abrahamA2StudentGuideTextGoldEn,
+  abrahamA2TeacherGuideGoldAr,
+  abrahamA2TeacherGuideGoldEn,
+  abrahamA2TeacherGuideMetadataGoldAr,
+  abrahamA2TeacherGuideMetadataGoldEn,
+} from './gold';
 
 export const abrahamA2BookDataEn: BookData = {
   id: 'a2-abraham-en',
   title: 'Stories of the Prophets: Abraham (A2)',
   level: 'A2',
   baseFontSize: 13,
-  pages: abrahamA2PagesEn,
-  teacherGuide: abrahamA2TeacherGuideEn,
-  teacherGuideMetadata: abrahamA2TeacherGuideMetadataEn,
-  selfStudyGuide: abrahamA2SelfStudyGuideEn,
-  studentGuideText: abrahamA2StudentGuideTextEn,
+  pages: abrahamA2PagesGoldEn,
+  teacherGuide: abrahamA2TeacherGuideGoldEn,
+  teacherGuideMetadata: abrahamA2TeacherGuideMetadataGoldEn,
+  selfStudyGuide: abrahamA2SelfStudyGuideGoldEn,
+  studentGuideSections: abrahamA2StudentGuideSectionsGoldEn,
+  studentGuideMetadata: abrahamA2StudentGuideMetadataGoldEn,
+  studentGuideText: abrahamA2StudentGuideTextGoldEn,
 };
 
 export const abrahamA2BookDataAr: BookData = {
   id: 'a2-abraham-ar',
-  title: 'قصص الأنبياء: إبراهيم (عليه السلام)',
+  title: 'قصص الأنبياء: إبراهيم (عليه السلام) (A2)',
   level: 'A2',
   baseFontSize: 14,
-  pages: abrahamA2PagesAr,
-  teacherGuide: abrahamA2TeacherGuideAr,
-  teacherGuideMetadata: abrahamA2TeacherGuideMetadataAr,
-  selfStudyGuide: abrahamA2SelfStudyGuideAr,
-  studentGuideText: abrahamA2StudentGuideTextAr,
+  pages: abrahamA2PagesGoldAr,
+  teacherGuide: abrahamA2TeacherGuideGoldAr,
+  teacherGuideMetadata: abrahamA2TeacherGuideMetadataGoldAr,
+  selfStudyGuide: abrahamA2SelfStudyGuideGoldAr,
+  studentGuideSections: abrahamA2StudentGuideSectionsGoldAr,
+  studentGuideMetadata: abrahamA2StudentGuideMetadataGoldAr,
+  studentGuideText: abrahamA2StudentGuideTextGoldAr,
 };
 
 export const abrahamA2BookData = abrahamA2BookDataEn;
