b8e421d6604862b9f0f9fd911d9affbaaedc2b83
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-08 12:13:53 +0300

    feat: wire Moses B2 Arabic Gold rollout

--- src/data/moses/b2/index.ts
+++ src/data/moses/b2/index.ts
@@ -8,14 +8,15 @@ import {
   mosesB2StudentGuideTextFinalEn,
   mosesB2StudentGuideMetadataFinalEn,
 } from './goldGuideFinal';
-
-import { mosesB2PagesAr } from './ar/pages';
-import { mosesB2TeacherGuideAr, mosesB2TeacherGuideMetadataAr } from './ar/teacherGuide';
-import { mosesB2SelfStudyGuideAr, mosesB2StudentGuideSectionsAr, mosesB2StudentGuideTextAr, mosesB2StudentGuideMetadataAr } from './ar/selfStudyGuide';
-
-import { mosesA2Pages } from '../a2/en/pages';
-import { mosesA2TeacherGuide, mosesA2TeacherGuideMetadata } from '../a2/en/teacherGuide';
-import { mosesA2SelfStudyGuide, mosesA2StudentGuideSections, mosesA2StudentGuideText, mosesA2StudentGuideMetadata } from '../a2/en/selfStudyGuide';
+import { mosesB2PagesGoldAr } from './goldLearningAr';
+import {
+  mosesB2TeacherGuideFinalAr,
+  mosesB2TeacherGuideMetadataFinalAr,
+  mosesB2SelfStudyGuideFinalAr,
+  mosesB2StudentGuideSectionsFinalAr,
+  mosesB2StudentGuideTextFinalAr,
+  mosesB2StudentGuideMetadataFinalAr,
+} from './goldGuideFinalAr';
 
 export const mosesB2BookDataEn: BookData = {
   id: 'moses-b2-en',
@@ -31,19 +32,18 @@ export const mosesB2BookDataEn: BookData = {
   studentGuideMetadata: mosesB2StudentGuideMetadataFinalEn,
 };
 
-// Arabic remains on the pre-rollout path until the English B2 pilot is accepted.
 export const mosesB2BookDataAr: BookData = {
   id: 'moses-b2-ar',
   title: 'قصص الأنبياء: موسى (عليه السلام) (B2)',
   level: 'B2',
   baseFontSize: 14,
-  pages: mosesB2PagesAr.length > 0 ? mosesB2PagesAr : mosesA2Pages,
-  teacherGuide: mosesB2TeacherGuideAr.length > 0 ? mosesB2TeacherGuideAr : mosesA2TeacherGuide,
-  teacherGuideMetadata: mosesB2TeacherGuideMetadataAr.targetLearners ? mosesB2TeacherGuideMetadataAr : mosesA2TeacherGuideMetadata,
-  selfStudyGuide: mosesB2SelfStudyGuideAr.length > 0 ? mosesB2SelfStudyGuideAr : mosesA2SelfStudyGuide,
-  studentGuideSections: mosesB2StudentGuideSectionsAr.length > 0 ? mosesB2StudentGuideSectionsAr : mosesA2StudentGuideSections,
-  studentGuideText: mosesB2StudentGuideTextAr || mosesA2StudentGuideText,
-  studentGuideMetadata: mosesB2StudentGuideMetadataAr.whoIsThisFor ? mosesB2StudentGuideMetadataAr : mosesA2StudentGuideMetadata,
+  pages: mosesB2PagesGoldAr,
+  teacherGuide: mosesB2TeacherGuideFinalAr,
+  teacherGuideMetadata: mosesB2TeacherGuideMetadataFinalAr,
+  selfStudyGuide: mosesB2SelfStudyGuideFinalAr,
+  studentGuideSections: mosesB2StudentGuideSectionsFinalAr,
+  studentGuideText: mosesB2StudentGuideTextFinalAr,
+  studentGuideMetadata: mosesB2StudentGuideMetadataFinalAr,
 };
 
 export const mosesB2BookData = mosesB2BookDataEn;
