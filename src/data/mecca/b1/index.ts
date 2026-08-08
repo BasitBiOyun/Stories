bb1ad2748537044bdaf90d77068af5771416a4ff
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-08 14:06:56 +0300

    fix: use chapter-aligned B1 guides for Mecca

--- src/data/mecca/b1/index.ts
+++ src/data/mecca/b1/index.ts
@@ -1,15 +1,14 @@
 import { BookData } from '../../../types';
+import { buildB1EvidenceGuides } from '../../b1GoldGuides';
 import { meccaB1TeacherGuideMetadata } from './en/teacherGuide';
-import { meccaB1SelfStudyGuide, meccaB1StudentGuideSections, meccaB1StudentGuideText, meccaB1StudentGuideMetadata } from './en/selfStudyGuide';
+import { meccaB1StudentGuideSections, meccaB1StudentGuideText, meccaB1StudentGuideMetadata } from './en/selfStudyGuide';
 
 import { meccaB1TeacherGuideMetadataAr } from './ar/teacherGuide';
-import { meccaB1SelfStudyGuideAr, meccaB1StudentGuideSectionsAr, meccaB1StudentGuideTextAr, meccaB1StudentGuideMetadataAr } from './ar/selfStudyGuide';
-import {
-  meccaB1PagesGoldEn,
-  meccaB1PagesGoldAr,
-  meccaB1TeacherGuideGoldEn,
-  meccaB1TeacherGuideGoldAr,
-} from './gold';
+import { meccaB1StudentGuideSectionsAr, meccaB1StudentGuideTextAr, meccaB1StudentGuideMetadataAr } from './ar/selfStudyGuide';
+import { meccaB1PagesGoldEn, meccaB1PagesGoldAr } from './gold';
+
+const meccaB1GuidesEn = buildB1EvidenceGuides(meccaB1PagesGoldEn, 'en');
+const meccaB1GuidesAr = buildB1EvidenceGuides(meccaB1PagesGoldAr, 'ar');
 
 export const meccaB1BookDataEn: BookData = {
   id: 'mecca-b1-en',
@@ -17,9 +16,9 @@ export const meccaB1BookDataEn: BookData = {
   level: 'B1',
   baseFontSize: 13,
   pages: meccaB1PagesGoldEn,
-  teacherGuide: meccaB1TeacherGuideGoldEn,
+  teacherGuide: meccaB1GuidesEn.teacherGuide,
   teacherGuideMetadata: meccaB1TeacherGuideMetadata,
-  selfStudyGuide: meccaB1SelfStudyGuide,
+  selfStudyGuide: meccaB1GuidesEn.selfStudyGuide,
   studentGuideSections: meccaB1StudentGuideSections,
   studentGuideText: meccaB1StudentGuideText,
   studentGuideMetadata: meccaB1StudentGuideMetadata,
@@ -31,9 +30,9 @@ export const meccaB1BookDataAr: BookData = {
   level: 'B1',
   baseFontSize: 14,
   pages: meccaB1PagesGoldAr,
-  teacherGuide: meccaB1TeacherGuideGoldAr,
+  teacherGuide: meccaB1GuidesAr.teacherGuide,
   teacherGuideMetadata: meccaB1TeacherGuideMetadataAr,
-  selfStudyGuide: meccaB1SelfStudyGuideAr,
+  selfStudyGuide: meccaB1GuidesAr.selfStudyGuide,
   studentGuideSections: meccaB1StudentGuideSectionsAr,
   studentGuideText: meccaB1StudentGuideTextAr,
   studentGuideMetadata: meccaB1StudentGuideMetadataAr,
