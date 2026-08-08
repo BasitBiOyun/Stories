898fc08a38b9da5bcc6cea8b63f45f22270e1a9d
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-08 11:36:55 +0300

    Assemble Moses B2 English Gold pilot

---
+++ src/data/moses/b2/gold.ts
@@ -0,0 +1,9 @@
+export { mosesB2PagesGoldEn, mosesB2GoldContract, mosesB2QuestionSpecs } from './goldLearning';
+export {
+  mosesB2TeacherGuideGoldEn,
+  mosesB2TeacherGuideMetadataGoldEn,
+  mosesB2SelfStudyGuideGoldEn,
+  mosesB2StudentGuideTextGoldEn,
+  mosesB2StudentGuideSectionsGoldEn,
+  mosesB2StudentGuideMetadataGoldEn,
+} from './goldGuides';
