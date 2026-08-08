b179b2f1f92aa2bb56080dbd617c9f04abf720a6
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-08 09:40:58 +0300

    Wire Moses B1 EN and AR to completed rollout layers

--- src/data/moses/b1/index.ts
+++ src/data/moses/b1/index.ts
@@ -1,15 +1,17 @@
 import { BookData } from '../../../types';
-import { mosesB1Pages } from './en/pages';
-import { mosesB1TeacherGuide, mosesB1TeacherGuideMetadata } from './en/teacherGuide';
+import { mosesB1TeacherGuideMetadata } from './en/teacherGuide';
 import { mosesB1SelfStudyGuide, mosesB1StudentGuideSections, mosesB1StudentGuideText, mosesB1StudentGuideMetadata } from './en/selfStudyGuide';
-import { mosesB1PagesFinalEn, mosesB1TeacherGuideFinalEn } from './goldFinal';
 
-import { mosesB1PagesAr } from './ar/pages';
-import { mosesB1TeacherGuideAr, mosesB1TeacherGuideMetadataAr } from './ar/teacherGuide';
+import { mosesB1TeacherGuideMetadataAr } from './ar/teacherGuide';
 import { mosesB1SelfStudyGuideAr, mosesB1StudentGuideSectionsAr, mosesB1StudentGuideTextAr, mosesB1StudentGuideMetadataAr } from './ar/selfStudyGuide';
+import {
+  mosesB1PagesRolloutEn,
+  mosesB1PagesRolloutAr,
+  mosesB1TeacherGuideRolloutEn,
+  mosesB1TeacherGuideRolloutAr,
+} from './rollout';
 
-import { mosesA2Pages } from '../a2/en/pages';
-import { mosesA2TeacherGuide, mosesA2TeacherGuideMetadata } from '../a2/en/teacherGuide';
+import { mosesA2TeacherGuideMetadata } from '../a2/en/teacherGuide';
 import { mosesA2SelfStudyGuide, mosesA2StudentGuideSections, mosesA2StudentGuideText, mosesA2StudentGuideMetadata } from '../a2/en/selfStudyGuide';
 
 export const mosesB1BookDataEn: BookData = {
@@ -17,8 +19,8 @@ export const mosesB1BookDataEn: BookData = {
   title: 'Stories of the Prophets: Moses (B1)',
   level: 'B1',
   baseFontSize: 13,
-  pages: mosesB1PagesFinalEn.length > 0 ? mosesB1PagesFinalEn : mosesB1Pages,
-  teacherGuide: mosesB1TeacherGuideFinalEn.length > 0 ? mosesB1TeacherGuideFinalEn : mosesB1TeacherGuide,
+  pages: mosesB1PagesRolloutEn,
+  teacherGuide: mosesB1TeacherGuideRolloutEn,
   teacherGuideMetadata: mosesB1TeacherGuideMetadata.targetLearners ? mosesB1TeacherGuideMetadata : mosesA2TeacherGuideMetadata,
   selfStudyGuide: mosesB1SelfStudyGuide.length > 0 ? mosesB1SelfStudyGuide : mosesA2SelfStudyGuide,
   studentGuideSections: mosesB1StudentGuideSections.length > 0 ? mosesB1StudentGuideSections : mosesA2StudentGuideSections,
@@ -31,8 +33,8 @@ export const mosesB1BookDataAr: BookData = {
   title: 'قصص الأنبياء: موسى (عليه السلام) (B1)',
   level: 'B1',
   baseFontSize: 14,
-  pages: mosesB1PagesAr.length > 0 ? mosesB1PagesAr : mosesA2Pages,
-  teacherGuide: mosesB1TeacherGuideAr.length > 0 ? mosesB1TeacherGuideAr : mosesA2TeacherGuide,
+  pages: mosesB1PagesRolloutAr,
+  teacherGuide: mosesB1TeacherGuideRolloutAr,
   teacherGuideMetadata: mosesB1TeacherGuideMetadataAr.targetLearners ? mosesB1TeacherGuideMetadataAr : mosesA2TeacherGuideMetadata,
   selfStudyGuide: mosesB1SelfStudyGuideAr.length > 0 ? mosesB1SelfStudyGuideAr : mosesA2SelfStudyGuide,
   studentGuideSections: mosesB1StudentGuideSectionsAr.length > 0 ? mosesB1StudentGuideSectionsAr : mosesA2StudentGuideSections,
