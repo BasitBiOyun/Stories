809ebcf58da0a202b229bf904e35e951b80e3b69
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-11 23:00:31 +0300

    Lock Adam B1 hotspots to chapter extracts

--- src/data/adam/b1/index.ts
+++ src/data/adam/b1/index.ts
@@ -1,5 +1,6 @@
 import { BookData } from '../../../types';
 import { buildB1EvidenceGuides } from '../../b1GoldGuides';
+import { applyHotspotSourceLock } from '../../storyHotspotSourceLock';
 import { adamB1TeacherGuideMetadata } from './en/teacherGuide';
 import { adamB1StudentGuideText, adamB1StudentGuideMetadata, adamB1StudentGuideSections } from './en/selfStudyGuide';
 
@@ -7,15 +8,17 @@ import { adamB1TeacherGuideMetadataAr } from './ar/teacherGuide';
 import { adamB1StudentGuideTextAr, adamB1StudentGuideMetadataAr, adamB1StudentGuideSectionsAr } from './ar/selfStudyGuide';
 import { adamB1PagesGoldEn, adamB1PagesGoldAr } from './gold';
 
-const adamB1GuidesEn = buildB1EvidenceGuides(adamB1PagesGoldEn, 'en');
-const adamB1GuidesAr = buildB1EvidenceGuides(adamB1PagesGoldAr, 'ar');
+const adamB1PagesLockedEn = applyHotspotSourceLock(adamB1PagesGoldEn, { language: 'en', level: 'B1' });
+const adamB1PagesLockedAr = applyHotspotSourceLock(adamB1PagesGoldAr, { language: 'ar', level: 'B1' });
+const adamB1GuidesEn = buildB1EvidenceGuides(adamB1PagesLockedEn, 'en');
+const adamB1GuidesAr = buildB1EvidenceGuides(adamB1PagesLockedAr, 'ar');
 
 export const adamB1BookDataEn: BookData = {
   id: 'b1-prophets-en',
   title: 'Stories of the Prophets: Adam (B1)',
   level: 'B1',
   baseFontSize: 12,
-  pages: adamB1PagesGoldEn,
+  pages: adamB1PagesLockedEn,
   teacherGuide: adamB1GuidesEn.teacherGuide,
   selfStudyGuide: adamB1GuidesEn.selfStudyGuide,
   studentGuideText: adamB1StudentGuideText,
@@ -29,7 +32,7 @@ export const adamB1BookDataAr: BookData = {
   title: 'قصص الأنبياء: آدم (عليه السلام)',
   level: 'B1',
   baseFontSize: 14,
-  pages: adamB1PagesGoldAr,
+  pages: adamB1PagesLockedAr,
   teacherGuide: adamB1GuidesAr.teacherGuide,
   selfStudyGuide: adamB1GuidesAr.selfStudyGuide,
   studentGuideText: adamB1StudentGuideTextAr,
