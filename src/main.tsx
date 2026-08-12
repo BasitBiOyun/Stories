8cef366633e0f0b607e110fdaf1eb7d66983034f
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-12 12:59:13 +0300

    wire fullscreen and collection guide themes

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './loading.css';
+import './guideTheme.css';
 import { LanguageProvider } from './contexts/LanguageContext';
+import { FullscreenButton } from './components/ui/FullscreenButton';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LanguageProvider>
       <App />
+      <FullscreenButton />
     </LanguageProvider>
   </StrictMode>,
 );
