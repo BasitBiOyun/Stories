4059946d22d22cd884cb848909d54caaa1dd0a48
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-12 13:06:29 +0300

    use Phosphor fullscreen glyph

--- src/components/ui/FullscreenButton.tsx
+++ src/components/ui/FullscreenButton.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
-import { Move } from './icons';
+
+// Phosphor Icons / CornersOut / Regular (MIT)
+const PhosphorCornersOut = ({ size = 19 }: { size?: number }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 256 256"
+    fill="currentColor"
+    aria-hidden="true"
+  >
+    <path d="M216,48V88a8,8,0,0,1-16,0V56H168a8,8,0,0,1,0-16h40A8,8,0,0,1,216,48ZM88,200H56V168a8,8,0,0,0-16,0v40a8,8,0,0,0,8,8H88a8,8,0,0,0,0-16Zm120-40a8,8,0,0,0-8,8v32H168a8,8,0,0,0,0,16h40a8,8,0,0,0,8-8V168A8,8,0,0,0,208,160ZM88,40H48a8,8,0,0,0-8,8V88a8,8,0,0,0,16,0V56H88a8,8,0,0,0,0-16Z" />
+  </svg>
+);
 
 export const FullscreenButton = () => {
   const [isSupported, setIsSupported] = React.useState(false);
@@ -33,7 +46,7 @@ export const FullscreenButton = () => {
       title="Full screen"
       aria-label="Full screen"
     >
-      <Move size={19} />
+      <PhosphorCornersOut />
     </button>
   );
 };
