1ff9240a7eb648ac6585e03f1e18081148e753ba
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-15 16:58:58 +0300

    apply canonical B1 highlights to Mecca

--- src/data/mecca/b1/index.ts
+++ src/data/mecca/b1/index.ts
@@ -1,5 +1,6 @@
 import { BookData } from '../../../types';
 import { buildB1EvidenceGuides } from '../../b1GoldGuides';
+import { applyB1HighlightStandard } from '../../b1HighlightStandard';
 import { applyHighlightSurfaceForms } from '../../highlightSourceLock';
 import { applyHotspotSourceLock } from '../../storyHotspotSourceLock';
 import { meccaB1TeacherGuideMetadata } from './en/teacherGuide';
@@ -7,7 +8,7 @@ import { meccaB1StudentGuideSections, meccaB1StudentGuideText, meccaB1StudentGui
 
 import { meccaB1TeacherGuideMetadataAr } from './ar/teacherGuide';
 import { meccaB1StudentGuideSectionsAr, meccaB1StudentGuideTextAr, meccaB1StudentGuideMetadataAr } from './ar/selfStudyGuide';
-import { meccaB1PagesGoldEn, meccaB1PagesGoldAr } from './gold';
+import { meccaB1GoldConfig, meccaB1PagesGoldEn, meccaB1PagesGoldAr } from './gold';
 
 const meccaB1PagesLockedEn = applyHotspotSourceLock(meccaB1PagesGoldEn, {
   language: 'en',
@@ -23,15 +24,26 @@ const meccaB1PagesLockedAr = applyHighlightSurfaceForms(
   applyHotspotSourceLock(meccaB1PagesGoldAr, { language: 'ar', level: 'B1' }),
   'ar',
 );
-const meccaB1GuidesEn = buildB1EvidenceGuides(meccaB1PagesLockedEn, 'en');
-const meccaB1GuidesAr = buildB1EvidenceGuides(meccaB1PagesLockedAr, 'ar');
+
+const meccaB1HighlightStandard = applyB1HighlightStandard(meccaB1PagesLockedEn, meccaB1PagesLockedAr, {
+  storyKey: 'Mecca',
+  storyIds: meccaB1GoldConfig.storyIds,
+  glossaryPageIds: meccaB1GoldConfig.glossaryPageIds,
+  vocabularyPageId: meccaB1GoldConfig.vocabularyPageId,
+});
+
+export const meccaB1HighlightTargets = meccaB1HighlightStandard.targets;
+const meccaB1PagesFinalEn = meccaB1HighlightStandard.englishPages;
+const meccaB1PagesFinalAr = meccaB1HighlightStandard.arabicPages;
+const meccaB1GuidesEn = buildB1EvidenceGuides(meccaB1PagesFinalEn, 'en');
+const meccaB1GuidesAr = buildB1EvidenceGuides(meccaB1PagesFinalAr, 'ar');
 
 export const meccaB1BookDataEn: BookData = {
   id: 'mecca-b1-en',
   title: 'Stories of the Prophets: Mecca (B1)',
   level: 'B1',
   baseFontSize: 13,
-  pages: meccaB1PagesLockedEn,
+  pages: meccaB1PagesFinalEn,
   teacherGuide: meccaB1GuidesEn.teacherGuide,
   teacherGuideMetadata: meccaB1TeacherGuideMetadata,
   selfStudyGuide: meccaB1GuidesEn.selfStudyGuide,
@@ -45,7 +57,7 @@ export const meccaB1BookDataAr: BookData = {
   title: 'قصص الأنبياء: مكة المكرمة (B1)',
   level: 'B1',
   baseFontSize: 14,
-  pages: meccaB1PagesLockedAr,
+  pages: meccaB1PagesFinalAr,
   teacherGuide: meccaB1GuidesAr.teacherGuide,
   teacherGuideMetadata: meccaB1TeacherGuideMetadataAr,
   selfStudyGuide: meccaB1GuidesAr.selfStudyGuide,
