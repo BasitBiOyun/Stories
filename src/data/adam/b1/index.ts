29ad17a3193482cc7483e4432c3b0c2c9fd0ebe8
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-16 16:24:53 +0300

    Use centralized Adam B1 source contract

--- src/data/adam/b1/index.ts
+++ src/data/adam/b1/index.ts
@@ -1,45 +1,18 @@
 import { BookData } from '../../../types';
-import { applyB1HighlightStandard } from '../../b1HighlightStandard';
 import { runB1BlueprintSystem } from '../../b1BlueprintSystem';
-import { applyHighlightSurfaceForms } from '../../highlightSourceLock';
-import { applyHotspotSourceLock } from '../../storyHotspotSourceLock';
 import { adamB1TeacherGuideMetadata } from './en/teacherGuide';
 import { adamB1StudentGuideText, adamB1StudentGuideMetadata, adamB1StudentGuideSections } from './en/selfStudyGuide';
-import { adamB1Pages } from './en/pages';
 import { adamB1TeacherGuideMetadataAr } from './ar/teacherGuide';
 import { adamB1StudentGuideTextAr, adamB1StudentGuideMetadataAr, adamB1StudentGuideSectionsAr } from './ar/selfStudyGuide';
-import { adamB1PagesAr } from './ar/pages';
-import { adamB1ExplicitHighlightTargets } from './highlights';
 import { adamB1LearningBlueprint } from './learningBlueprint';
 import { adamB1BlueprintConfig } from './config';
+import { adamB1SourcePagesAr, adamB1SourcePagesEn, adamB1HighlightTargets } from './source';
 
-const adamB1PagesLockedEn = applyHotspotSourceLock(adamB1Pages, {
-  language: 'en',
-  level: 'B1',
-  titleOverrides: {
-    10: { 'h10-1': 'Sheep', 'h10-2': 'Crops' },
-    11: { 'h11-1': 'Raven', 'h11-2': 'Anger' },
-  },
-});
-
-const adamB1PagesLockedAr = applyHighlightSurfaceForms(
-  applyHotspotSourceLock(adamB1PagesAr, { language: 'ar', level: 'B1' }),
-  'ar',
-);
-
-const adamB1HighlightStandard = applyB1HighlightStandard(adamB1PagesLockedEn, adamB1PagesLockedAr, {
-  storyKey: 'Adam',
-  storyIds: adamB1BlueprintConfig.storyIds,
-  glossaryPageIds: adamB1BlueprintConfig.glossaryPageIds,
-  vocabularyPageId: adamB1BlueprintConfig.vocabularyPageId,
-  explicitTargets: adamB1ExplicitHighlightTargets,
-});
-
-export const adamB1HighlightTargets = adamB1HighlightStandard.targets;
+export { adamB1HighlightTargets };
 
 const adamB1Blueprint = runB1BlueprintSystem({
-  englishPages: adamB1HighlightStandard.englishPages,
-  arabicPages: adamB1HighlightStandard.arabicPages,
+  englishPages: adamB1SourcePagesEn,
+  arabicPages: adamB1SourcePagesAr,
   config: adamB1BlueprintConfig,
   blueprint: adamB1LearningBlueprint,
 });
