944ca3a7fd176c7a0cb54d793617e65ff6ba93c7
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-16 16:14:08 +0300

    Migrate Adam B1 runtime to Blueprint

--- src/data/adam/b1/index.ts
+++ src/data/adam/b1/index.ts
@@ -1,18 +1,19 @@
 import { BookData } from '../../../types';
-import { buildB1EvidenceGuides } from '../../b1GoldGuides';
 import { applyB1HighlightStandard } from '../../b1HighlightStandard';
-import { applySafeAdvancedParallelLearning } from '../../safeAdvancedParallelLearning';
+import { runB1BlueprintSystem } from '../../b1BlueprintSystem';
 import { applyHighlightSurfaceForms } from '../../highlightSourceLock';
 import { applyHotspotSourceLock } from '../../storyHotspotSourceLock';
 import { adamB1TeacherGuideMetadata } from './en/teacherGuide';
 import { adamB1StudentGuideText, adamB1StudentGuideMetadata, adamB1StudentGuideSections } from './en/selfStudyGuide';
-
+import { adamB1Pages } from './en/pages';
 import { adamB1TeacherGuideMetadataAr } from './ar/teacherGuide';
 import { adamB1StudentGuideTextAr, adamB1StudentGuideMetadataAr, adamB1StudentGuideSectionsAr } from './ar/selfStudyGuide';
-import { adamB1GoldConfig, adamB1PagesGoldEn, adamB1PagesGoldAr } from './gold';
+import { adamB1PagesAr } from './ar/pages';
 import { adamB1ExplicitHighlightTargets } from './highlights';
+import { adamB1LearningBlueprint } from './learningBlueprint';
+import { adamB1BlueprintConfig } from './config';
 
-const adamB1PagesLockedEn = applyHotspotSourceLock(adamB1PagesGoldEn, {
+const adamB1PagesLockedEn = applyHotspotSourceLock(adamB1Pages, {
   language: 'en',
   level: 'B1',
   titleOverrides: {
@@ -20,40 +21,41 @@ const adamB1PagesLockedEn = applyHotspotSourceLock(adamB1PagesGoldEn, {
     11: { 'h11-1': 'Raven', 'h11-2': 'Anger' },
   },
 });
+
 const adamB1PagesLockedAr = applyHighlightSurfaceForms(
-  applyHotspotSourceLock(adamB1PagesGoldAr, { language: 'ar', level: 'B1' }),
+  applyHotspotSourceLock(adamB1PagesAr, { language: 'ar', level: 'B1' }),
   'ar',
 );
 
 const adamB1HighlightStandard = applyB1HighlightStandard(adamB1PagesLockedEn, adamB1PagesLockedAr, {
   storyKey: 'Adam',
-  storyIds: adamB1GoldConfig.storyIds,
-  glossaryPageIds: adamB1GoldConfig.glossaryPageIds,
-  vocabularyPageId: adamB1GoldConfig.vocabularyPageId,
+  storyIds: adamB1BlueprintConfig.storyIds,
+  glossaryPageIds: adamB1BlueprintConfig.glossaryPageIds,
+  vocabularyPageId: adamB1BlueprintConfig.vocabularyPageId,
   explicitTargets: adamB1ExplicitHighlightTargets,
 });
 
 export const adamB1HighlightTargets = adamB1HighlightStandard.targets;
-const adamB1Parallel = applySafeAdvancedParallelLearning({
+
+const adamB1Blueprint = runB1BlueprintSystem({
   englishPages: adamB1HighlightStandard.englishPages,
   arabicPages: adamB1HighlightStandard.arabicPages,
-  config: { level: 'B1', ...adamB1GoldConfig },
+  config: adamB1BlueprintConfig,
+  blueprint: adamB1LearningBlueprint,
 });
-const adamB1GuidesEn = buildB1EvidenceGuides(adamB1Parallel.englishPages, 'en');
-const adamB1GuidesAr = buildB1EvidenceGuides(adamB1Parallel.arabicPages, 'ar');
 
 export const adamB1BookDataEn: BookData = {
   id: 'b1-prophets-en',
   title: 'Stories of the Prophets: Adam (B1)',
   level: 'B1',
   baseFontSize: 12,
-  pages: adamB1Parallel.englishPages,
-  teacherGuide: adamB1GuidesEn.teacherGuide,
-  selfStudyGuide: adamB1GuidesEn.selfStudyGuide,
+  pages: adamB1Blueprint.englishPages,
+  teacherGuide: adamB1Blueprint.englishTeacherGuide,
+  selfStudyGuide: adamB1Blueprint.englishSelfStudyGuide,
   studentGuideText: adamB1StudentGuideText,
   studentGuideSections: adamB1StudentGuideSections,
   teacherGuideMetadata: adamB1TeacherGuideMetadata,
-  studentGuideMetadata: adamB1StudentGuideMetadata
+  studentGuideMetadata: adamB1StudentGuideMetadata,
 };
 
 export const adamB1BookDataAr: BookData = {
@@ -61,11 +63,11 @@ export const adamB1BookDataAr: BookData = {
   title: 'قصص الأنبياء: آدم (عليه السلام)',
   level: 'B1',
   baseFontSize: 14,
-  pages: adamB1Parallel.arabicPages,
-  teacherGuide: adamB1GuidesAr.teacherGuide,
-  selfStudyGuide: adamB1GuidesAr.selfStudyGuide,
+  pages: adamB1Blueprint.arabicPages,
+  teacherGuide: adamB1Blueprint.arabicTeacherGuide,
+  selfStudyGuide: adamB1Blueprint.arabicSelfStudyGuide,
   studentGuideText: adamB1StudentGuideTextAr,
   studentGuideSections: adamB1StudentGuideSectionsAr,
   teacherGuideMetadata: adamB1TeacherGuideMetadataAr,
-  studentGuideMetadata: adamB1StudentGuideMetadataAr
+  studentGuideMetadata: adamB1StudentGuideMetadataAr,
 };
