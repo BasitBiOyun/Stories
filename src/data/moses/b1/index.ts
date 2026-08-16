407d2509402059cdf5c29ad7c539b1b1be449068
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-16 16:36:06 +0300

    Migrate Moses B1 runtime to Blueprint

--- src/data/moses/b1/index.ts
+++ src/data/moses/b1/index.ts
@@ -1,57 +1,34 @@
 import { BookData } from '../../../types';
-import { buildB1EvidenceGuides } from '../../b1GoldGuides';
-import { applyB1HighlightStandard } from '../../b1HighlightStandard';
-import { applySafeAdvancedParallelLearning } from '../../safeAdvancedParallelLearning';
-import { applyHotspotSourceLock } from '../../storyHotspotSourceLock';
+import { runB1BlueprintSystem } from '../../b1BlueprintSystem';
 import { mosesB1TeacherGuideMetadata } from './en/teacherGuide';
 import { mosesB1StudentGuideSections, mosesB1StudentGuideText, mosesB1StudentGuideMetadata } from './en/selfStudyGuide';
-
 import { mosesB1TeacherGuideMetadataAr } from './ar/teacherGuide';
 import { mosesB1StudentGuideSectionsAr, mosesB1StudentGuideTextAr, mosesB1StudentGuideMetadataAr } from './ar/selfStudyGuide';
-import { mosesB1PagesRolloutEn, mosesB1PagesRolloutAr, mosesB1RolloutConfig } from './rollout';
-import { mosesB1ExplicitHighlightTargets } from './highlights';
+import { mosesB1LearningBlueprint } from './learningBlueprint';
+import { mosesB1BlueprintConfig } from './config';
+import { mosesB1HighlightTargets, mosesB1SourcePagesAr, mosesB1SourcePagesEn } from './source';
 
 import { mosesA2TeacherGuideMetadata } from '../a2/en/teacherGuide';
 import { mosesA2StudentGuideSections, mosesA2StudentGuideText, mosesA2StudentGuideMetadata } from '../a2/en/selfStudyGuide';
 
-const mosesB1PagesLockedEn = applyHotspotSourceLock(mosesB1PagesRolloutEn, {
-  language: 'en',
-  level: 'B1',
-  titleOverrides: {
-    4: { 'h4-1': 'Mother', 'h4-2': 'Queen Asiye' },
-    9: { 'h9-1': 'Voice' },
-    11: { 'h11-2': 'Staff' },
-    12: { 'h12-1': 'Night' },
-  },
-});
-const mosesB1PagesLockedAr = applyHotspotSourceLock(mosesB1PagesRolloutAr, { language: 'ar', level: 'B1' });
+export { mosesB1HighlightTargets };
 
-const mosesB1HighlightStandard = applyB1HighlightStandard(mosesB1PagesLockedEn, mosesB1PagesLockedAr, {
-  storyKey: 'Moses',
-  storyIds: mosesB1RolloutConfig.storyIds,
-  glossaryPageIds: mosesB1RolloutConfig.glossaryPageIds,
-  vocabularyPageId: mosesB1RolloutConfig.vocabularyPageId,
-  explicitTargets: mosesB1ExplicitHighlightTargets,
+const mosesB1Blueprint = runB1BlueprintSystem({
+  englishPages: mosesB1SourcePagesEn,
+  arabicPages: mosesB1SourcePagesAr,
+  config: mosesB1BlueprintConfig,
+  blueprint: mosesB1LearningBlueprint,
 });
-
-export const mosesB1HighlightTargets = mosesB1HighlightStandard.targets;
-const mosesB1Parallel = applySafeAdvancedParallelLearning({
-  englishPages: mosesB1HighlightStandard.englishPages,
-  arabicPages: mosesB1HighlightStandard.arabicPages,
-  config: { level: 'B1', ...mosesB1RolloutConfig },
-});
-const mosesB1GuidesEn = buildB1EvidenceGuides(mosesB1Parallel.englishPages, 'en');
-const mosesB1GuidesAr = buildB1EvidenceGuides(mosesB1Parallel.arabicPages, 'ar');
 
 export const mosesB1BookDataEn: BookData = {
   id: 'moses-b1-en',
   title: 'Stories of the Prophets: Moses (B1)',
   level: 'B1',
   baseFontSize: 13,
-  pages: mosesB1Parallel.englishPages,
-  teacherGuide: mosesB1GuidesEn.teacherGuide,
+  pages: mosesB1Blueprint.englishPages,
+  teacherGuide: mosesB1Blueprint.englishTeacherGuide,
   teacherGuideMetadata: mosesB1TeacherGuideMetadata.targetLearners ? mosesB1TeacherGuideMetadata : mosesA2TeacherGuideMetadata,
-  selfStudyGuide: mosesB1GuidesEn.selfStudyGuide,
+  selfStudyGuide: mosesB1Blueprint.englishSelfStudyGuide,
   studentGuideSections: mosesB1StudentGuideSections.length > 0 ? mosesB1StudentGuideSections : mosesA2StudentGuideSections,
   studentGuideText: mosesB1StudentGuideText || mosesA2StudentGuideText,
   studentGuideMetadata: mosesB1StudentGuideMetadata.whoIsThisFor ? mosesB1StudentGuideMetadata : mosesA2StudentGuideMetadata,
@@ -62,10 +39,10 @@ export const mosesB1BookDataAr: BookData = {
   title: 'قصص الأنبياء: موسى (عليه السلام) (B1)',
   level: 'B1',
   baseFontSize: 14,
-  pages: mosesB1Parallel.arabicPages,
-  teacherGuide: mosesB1GuidesAr.teacherGuide,
+  pages: mosesB1Blueprint.arabicPages,
+  teacherGuide: mosesB1Blueprint.arabicTeacherGuide,
   teacherGuideMetadata: mosesB1TeacherGuideMetadataAr.targetLearners ? mosesB1TeacherGuideMetadataAr : mosesA2TeacherGuideMetadata,
-  selfStudyGuide: mosesB1GuidesAr.selfStudyGuide,
+  selfStudyGuide: mosesB1Blueprint.arabicSelfStudyGuide,
   studentGuideSections: mosesB1StudentGuideSectionsAr.length > 0 ? mosesB1StudentGuideSectionsAr : mosesA2StudentGuideSections,
   studentGuideText: mosesB1StudentGuideTextAr || mosesA2StudentGuideText,
   studentGuideMetadata: mosesB1StudentGuideMetadataAr.whoIsThisFor ? mosesB1StudentGuideMetadataAr : mosesA2StudentGuideMetadata,
