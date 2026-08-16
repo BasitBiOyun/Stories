528a00e4c12973651f1680b845a90c63c492f850
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-16 19:21:23 +0300

    Migrate Yunus Emre B1 runtime to Blueprint

--- src/data/yunusEmre/b1/index.ts
+++ src/data/yunusEmre/b1/index.ts
@@ -1,58 +1,31 @@
 import { BookData } from '../../../types';
-import { buildB1EvidenceGuides } from '../../b1GoldGuides';
-import { applyB1HighlightStandard } from '../../b1HighlightStandard';
-import { applySafeAdvancedParallelLearning } from '../../safeAdvancedParallelLearning';
-import { stripUnsupportedBoldMarkdown } from '../../stripUnsupportedMarkdown';
-import { applyHotspotSourceLock } from '../../storyHotspotSourceLock';
+import { runB1BlueprintSystem } from '../../b1BlueprintSystem';
 import { yunusB1TeacherGuideMetadata } from './en/teacherGuide';
 import { yunusB1StudentGuideSections, yunusB1StudentGuideText, yunusB1StudentGuideMetadata } from './en/selfStudyGuide';
-
 import { yunusEmreB1TeacherGuideMetadataAr } from './ar/teacherGuide';
 import { yunusEmreB1StudentGuideSectionsAr, yunusEmreB1StudentGuideTextAr, yunusEmreB1StudentGuideMetadataAr } from './ar/selfStudyGuide';
-import { yunusEmreB1GoldConfig, yunusEmreB1PagesGoldEn, yunusEmreB1PagesGoldAr } from './gold';
-import { yunusEmreB1ExplicitHighlightTargets } from './highlights';
+import { yunusEmreB1LearningBlueprint } from './learningBlueprint';
+import { yunusEmreB1BlueprintConfig } from './config';
+import { yunusEmreB1HighlightTargets, yunusEmreB1SourcePagesAr, yunusEmreB1SourcePagesEn } from './source';
 
-const yunusB1PagesLockedEn = stripUnsupportedBoldMarkdown(applyHotspotSourceLock(yunusEmreB1PagesGoldEn, {
-  language: 'en',
-  level: 'B1',
-  titleOverrides: {
-    3: { 'h3-2': 'Anatolia' },
-    4: { 'h4-1': 'Anatolian Seljuk' },
-    6: { 'h6-1': 'Raids' },
-    7: { 'h7-1': 'Wise Sûfî', 'h7-2': 'Poetry' },
-    11: { 'h11-1': 'Intellect', 'h11-2': 'Death' },
-    12: { 'h12-2': 'Moral Principles' },
-    13: { 'h13-2': 'Patience' },
-  },
-}));
-const yunusB1PagesLockedAr = applyHotspotSourceLock(yunusEmreB1PagesGoldAr, { language: 'ar', level: 'B1' });
+export { yunusEmreB1HighlightTargets };
 
-const yunusB1HighlightStandard = applyB1HighlightStandard(yunusB1PagesLockedEn, yunusB1PagesLockedAr, {
-  storyKey: 'Yunus Emre',
-  storyIds: yunusEmreB1GoldConfig.storyIds,
-  glossaryPageIds: yunusEmreB1GoldConfig.glossaryPageIds,
-  vocabularyPageId: yunusEmreB1GoldConfig.vocabularyPageId,
-  explicitTargets: yunusEmreB1ExplicitHighlightTargets,
+const yunusEmreB1Blueprint = runB1BlueprintSystem({
+  englishPages: yunusEmreB1SourcePagesEn,
+  arabicPages: yunusEmreB1SourcePagesAr,
+  config: yunusEmreB1BlueprintConfig,
+  blueprint: yunusEmreB1LearningBlueprint,
 });
-
-export const yunusEmreB1HighlightTargets = yunusB1HighlightStandard.targets;
-const yunusB1Parallel = applySafeAdvancedParallelLearning({
-  englishPages: yunusB1HighlightStandard.englishPages,
-  arabicPages: yunusB1HighlightStandard.arabicPages,
-  config: { level: 'B1', ...yunusEmreB1GoldConfig },
-});
-const yunusB1GuidesEn = buildB1EvidenceGuides(yunusB1Parallel.englishPages, 'en');
-const yunusB1GuidesAr = buildB1EvidenceGuides(yunusB1Parallel.arabicPages, 'ar');
 
 export const yunusEmreB1BookDataEn: BookData = {
   id: 'yunusEmre-b1-en',
   title: 'Stories of the Prophets: Yunus Emre (B1)',
   level: 'B1',
   baseFontSize: 13,
-  pages: yunusB1Parallel.englishPages,
-  teacherGuide: yunusB1GuidesEn.teacherGuide,
+  pages: yunusEmreB1Blueprint.englishPages,
+  teacherGuide: yunusEmreB1Blueprint.englishTeacherGuide,
   teacherGuideMetadata: yunusB1TeacherGuideMetadata,
-  selfStudyGuide: yunusB1GuidesEn.selfStudyGuide,
+  selfStudyGuide: yunusEmreB1Blueprint.englishSelfStudyGuide,
   studentGuideSections: yunusB1StudentGuideSections,
   studentGuideText: yunusB1StudentGuideText,
   studentGuideMetadata: yunusB1StudentGuideMetadata,
@@ -63,10 +36,10 @@ export const yunusEmreB1BookDataAr: BookData = {
   title: 'قصص الأنبياء: يونس إمره (B1)',
   level: 'B1',
   baseFontSize: 14,
-  pages: yunusB1Parallel.arabicPages,
-  teacherGuide: yunusB1GuidesAr.teacherGuide,
+  pages: yunusEmreB1Blueprint.arabicPages,
+  teacherGuide: yunusEmreB1Blueprint.arabicTeacherGuide,
   teacherGuideMetadata: yunusEmreB1TeacherGuideMetadataAr,
-  selfStudyGuide: yunusB1GuidesAr.selfStudyGuide,
+  selfStudyGuide: yunusEmreB1Blueprint.arabicSelfStudyGuide,
   studentGuideSections: yunusEmreB1StudentGuideSectionsAr,
   studentGuideText: yunusEmreB1StudentGuideTextAr,
   studentGuideMetadata: yunusEmreB1StudentGuideMetadataAr,
