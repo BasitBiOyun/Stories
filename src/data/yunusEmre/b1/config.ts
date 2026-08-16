b71a19777b5ec4d0a359ca5d42f930800e648615
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-16 19:18:14 +0300

    Add Yunus Emre B1 Blueprint config

---
+++ src/data/yunusEmre/b1/config.ts
@@ -0,0 +1,8 @@
+export const yunusEmreB1BlueprintConfig = {
+  storyIds: Array.from({ length: 13 }, (_, index) => index + 1),
+  knowledgeCheckPageId: 14,
+  vocabularyPageId: 15,
+  glossaryPageIds: [16, 17] as [number, number],
+  reviewPageId: 18,
+  finalChallengePageId: 19,
+};
