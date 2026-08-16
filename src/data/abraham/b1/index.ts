0525ebb6a81dc115692e554bfeb81fd8c6bcbd81
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-16 16:53:37 +0300

    Migrate Abraham B1 runtime to Blueprint

--- src/data/abraham/b1/index.ts
+++ src/data/abraham/b1/index.ts
@@ -1,57 +1,34 @@
 import { BookData } from '../../../types';
-import { buildB1EvidenceGuides } from '../../b1GoldGuides';
-import { applyB1HighlightStandard } from '../../b1HighlightStandard';
-import { applySafeAdvancedParallelLearning } from '../../safeAdvancedParallelLearning';
-import { applyHotspotSourceLock } from '../../storyHotspotSourceLock';
+import { runB1BlueprintSystem } from '../../b1BlueprintSystem';
 import { abrahamB1TeacherGuideMetadata } from './en/teacherGuide';
 import { abrahamB1StudentGuideText, abrahamB1StudentGuideMetadata, abrahamB1StudentGuideSections } from './en/selfstudyGuide';
-
 import { abrahamB1TeacherGuideMetadataAr } from './ar/teacherGuide';
 import { abrahamB1StudentGuideTextAr, abrahamB1StudentGuideMetadataAr, abrahamB1StudentGuideSectionsAr } from './ar/selfStudyGuide';
-import { abrahamB1GoldConfig, abrahamB1PagesGoldEn, abrahamB1PagesGoldAr } from './gold';
-import { abrahamB1ExplicitHighlightTargets } from './highlights';
+import { abrahamB1LearningBlueprint } from './learningBlueprint';
+import { abrahamB1BlueprintConfig } from './config';
+import { abrahamB1HighlightTargets, abrahamB1SourcePagesAr, abrahamB1SourcePagesEn } from './source';
 
-const abrahamB1PagesLockedEn = applyHotspotSourceLock(abrahamB1PagesGoldEn, {
-  language: 'en',
-  level: 'B1',
-  titleOverrides: {
-    5: { 'h5-1': 'Idols', 'h5-2': 'People' },
-    6: { 'h6-1': 'Axe', 'h6-2': 'Food' },
-    7: { 'h7-1': 'Statues' },
-    13: { 'h13-2': 'Arabian Peninsula' },
-  },
-});
-const abrahamB1PagesLockedAr = applyHotspotSourceLock(abrahamB1PagesGoldAr, { language: 'ar', level: 'B1' });
+export { abrahamB1HighlightTargets };
 
-const abrahamB1HighlightStandard = applyB1HighlightStandard(abrahamB1PagesLockedEn, abrahamB1PagesLockedAr, {
-  storyKey: 'Abraham',
-  storyIds: abrahamB1GoldConfig.storyIds,
-  glossaryPageIds: abrahamB1GoldConfig.glossaryPageIds,
-  vocabularyPageId: abrahamB1GoldConfig.vocabularyPageId,
-  explicitTargets: abrahamB1ExplicitHighlightTargets,
+const abrahamB1Blueprint = runB1BlueprintSystem({
+  englishPages: abrahamB1SourcePagesEn,
+  arabicPages: abrahamB1SourcePagesAr,
+  config: abrahamB1BlueprintConfig,
+  blueprint: abrahamB1LearningBlueprint,
 });
-
-export const abrahamB1HighlightTargets = abrahamB1HighlightStandard.targets;
-const abrahamB1Parallel = applySafeAdvancedParallelLearning({
-  englishPages: abrahamB1HighlightStandard.englishPages,
-  arabicPages: abrahamB1HighlightStandard.arabicPages,
-  config: { level: 'B1', ...abrahamB1GoldConfig },
-});
-const abrahamB1GuidesEn = buildB1EvidenceGuides(abrahamB1Parallel.englishPages, 'en');
-const abrahamB1GuidesAr = buildB1EvidenceGuides(abrahamB1Parallel.arabicPages, 'ar');
 
 export const abrahamB1BookDataEn: BookData = {
   id: 'b1-abraham-en',
   title: 'Stories of the Prophets: Abraham (B1)',
   level: 'B1',
   baseFontSize: 12,
-  pages: abrahamB1Parallel.englishPages,
-  teacherGuide: abrahamB1GuidesEn.teacherGuide,
-  selfStudyGuide: abrahamB1GuidesEn.selfStudyGuide,
+  pages: abrahamB1Blueprint.englishPages,
+  teacherGuide: abrahamB1Blueprint.englishTeacherGuide,
+  selfStudyGuide: abrahamB1Blueprint.englishSelfStudyGuide,
   studentGuideText: abrahamB1StudentGuideText,
   studentGuideSections: abrahamB1StudentGuideSections,
   teacherGuideMetadata: abrahamB1TeacherGuideMetadata,
-  studentGuideMetadata: abrahamB1StudentGuideMetadata
+  studentGuideMetadata: abrahamB1StudentGuideMetadata,
 };
 
 export const abrahamB1BookDataAr: BookData = {
@@ -59,13 +36,13 @@ export const abrahamB1BookDataAr: BookData = {
   title: 'قصص الأنبياء: إبراهيم (عليه السلام) (B1)',
   level: 'B1',
   baseFontSize: 14,
-  pages: abrahamB1Parallel.arabicPages,
-  teacherGuide: abrahamB1GuidesAr.teacherGuide,
-  selfStudyGuide: abrahamB1GuidesAr.selfStudyGuide,
+  pages: abrahamB1Blueprint.arabicPages,
+  teacherGuide: abrahamB1Blueprint.arabicTeacherGuide,
+  selfStudyGuide: abrahamB1Blueprint.arabicSelfStudyGuide,
   studentGuideText: abrahamB1StudentGuideTextAr,
   studentGuideSections: abrahamB1StudentGuideSectionsAr,
   teacherGuideMetadata: abrahamB1TeacherGuideMetadataAr,
-  studentGuideMetadata: abrahamB1StudentGuideMetadataAr
+  studentGuideMetadata: abrahamB1StudentGuideMetadataAr,
 };
 
 export const abrahamB1BookData = abrahamB1BookDataEn;
