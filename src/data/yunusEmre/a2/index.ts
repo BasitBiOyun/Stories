30f47e72db9e40906822f2b6c708bfcf3b11c17e
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-18 19:57:29 +0300

    Wire Yunus Emre A2 Gold learning package

--- src/data/yunusEmre/a2/index.ts
+++ src/data/yunusEmre/a2/index.ts
@@ -1,25 +1,26 @@
-import { BookData } from '../../../types';
+import { BookData, PageData } from '../../../types';
 import { applyA2FinalStoryLanguageLock } from '../../a2FinalStoryLanguageLock';
-import { syncA2GlossariesFromStoryHighlights, validateA2HighlightStandard } from '../../a2HighlightStandard';
+import { syncA2GlossariesFromStoryHighlights } from '../../a2HighlightStandard';
 import { applyA2HotspotCopyOverrides } from '../../a2HotspotCopyOverrides';
 import { runA2BlueprintSystem } from '../../a2BlueprintSystem';
 import { applyYunusA2PoemCard } from './poemCard';
 import { yunusA2GoldConfig } from './gold';
-import { yunusA2LearningBlueprint } from './learningBlueprint';
+import { yunusA2GoldLearningBlueprint } from './goldLearningBlueprint';
+import {
+  applyYunusA2GoldVocabularyChallenge,
+  buildYunusA2GoldStudentGuideSections,
+  buildYunusA2GoldTeacherGuideMetadata,
+} from './goldGuides';
 import {
   yunusA2HighlightConfig,
-  yunusA2HighlightTargets,
   yunusA2PagesFinalAr,
   yunusA2PagesFinalEn,
   yunusA2StudentGuideMetadataFinalAr,
   yunusA2StudentGuideMetadataFinalEn,
-  yunusA2StudentGuideSectionsFinalAr,
-  yunusA2StudentGuideSectionsFinalEn,
-  yunusA2StudentGuideTextFinalAr,
-  yunusA2StudentGuideTextFinalEn,
   yunusA2TeacherGuideMetadataFinalAr,
   yunusA2TeacherGuideMetadataFinalEn,
 } from './goldFinal';
+import { buildYunusA2GoldStudentGuideText } from './studentGuideText';
 
 const yunusA2PagesLockedEn = applyYunusA2PoemCard(
   syncA2GlossariesFromStoryHighlights(
@@ -44,27 +45,51 @@ const yunusA2PagesLockedAr = applyA2HotspotCopyOverrides(
   },
 );
 
-validateA2HighlightStandard(yunusA2PagesLockedEn, yunusA2PagesLockedAr, yunusA2HighlightTargets, yunusA2HighlightConfig);
-
 const yunusA2 = runA2BlueprintSystem({
   englishPages: yunusA2PagesLockedEn,
   arabicPages: yunusA2PagesLockedAr,
-  config: { ...yunusA2GoldConfig, vocabularyPageId: 10 },
-  blueprint: yunusA2LearningBlueprint,
+  config: yunusA2GoldConfig,
+  blueprint: yunusA2GoldLearningBlueprint,
 });
+
+const restoreReviewActivities = (generatedPages: PageData[], sourcePages: PageData[]): PageData[] => {
+  const sourceReview = sourcePages.find(page => page.id === yunusA2GoldConfig.reviewPageId);
+  if (!sourceReview?.exercises?.length) return generatedPages;
+  return generatedPages.map(page => page.id === yunusA2GoldConfig.reviewPageId
+    ? { ...page, exercises: sourceReview.exercises }
+    : page);
+};
+
+const goldPagesEn = applyYunusA2GoldVocabularyChallenge(
+  restoreReviewActivities(yunusA2.englishPages, yunusA2PagesLockedEn),
+  yunusA2GoldLearningBlueprint,
+  'en',
+);
+const goldPagesAr = applyYunusA2GoldVocabularyChallenge(
+  restoreReviewActivities(yunusA2.arabicPages, yunusA2PagesLockedAr),
+  yunusA2GoldLearningBlueprint,
+  'ar',
+);
+
+const teacherMetadataEn = buildYunusA2GoldTeacherGuideMetadata(yunusA2TeacherGuideMetadataFinalEn, 'en');
+const teacherMetadataAr = buildYunusA2GoldTeacherGuideMetadata(yunusA2TeacherGuideMetadataFinalAr, 'ar');
+const studentSectionsEn = buildYunusA2GoldStudentGuideSections('en');
+const studentSectionsAr = buildYunusA2GoldStudentGuideSections('ar');
+const studentTextEn = buildYunusA2GoldStudentGuideText(yunusA2GoldLearningBlueprint, 'en');
+const studentTextAr = buildYunusA2GoldStudentGuideText(yunusA2GoldLearningBlueprint, 'ar');
 
 export const yunusEmreA2BookDataEn: BookData = {
   id: 'yunusEmre-a2-en',
   title: 'Yunus Emre: Faith, Character, and Poetry (A2)',
   level: 'A2',
   baseFontSize: 13,
-  pages: yunusA2.englishPages,
+  pages: goldPagesEn,
   teacherGuide: yunusA2.englishTeacherGuide,
-  teacherGuideMetadata: yunusA2TeacherGuideMetadataFinalEn,
+  teacherGuideMetadata: teacherMetadataEn,
   selfStudyGuide: yunusA2.englishSelfStudyGuide,
-  studentGuideSections: yunusA2StudentGuideSectionsFinalEn,
+  studentGuideSections: studentSectionsEn,
   studentGuideMetadata: yunusA2StudentGuideMetadataFinalEn,
-  studentGuideText: yunusA2StudentGuideTextFinalEn,
+  studentGuideText: studentTextEn,
 };
 
 export const yunusEmreA2BookDataAr: BookData = {
@@ -72,13 +97,13 @@ export const yunusEmreA2BookDataAr: BookData = {
   title: 'يونس إمره: الإيمان والأخلاق والشعر (A2)',
   level: 'A2',
   baseFontSize: 14,
-  pages: yunusA2.arabicPages,
+  pages: goldPagesAr,
   teacherGuide: yunusA2.arabicTeacherGuide,
-  teacherGuideMetadata: yunusA2TeacherGuideMetadataFinalAr,
+  teacherGuideMetadata: teacherMetadataAr,
   selfStudyGuide: yunusA2.arabicSelfStudyGuide,
-  studentGuideSections: yunusA2StudentGuideSectionsFinalAr,
+  studentGuideSections: studentSectionsAr,
   studentGuideMetadata: yunusA2StudentGuideMetadataFinalAr,
-  studentGuideText: yunusA2StudentGuideTextFinalAr,
+  studentGuideText: studentTextAr,
 };
 
 export const yunusEmreA2BookData = yunusEmreA2BookDataEn;
