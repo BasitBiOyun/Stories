32675488854fc55eb507277c35a77d723a289002
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-18 19:10:34 +0300

    Add Moses A2 to structure check

--- scripts/validation/validateLearningStructure.ts
+++ scripts/validation/validateLearningStructure.ts
@@ -1,4 +1,5 @@
 import { adamA2BookDataAr, adamA2BookDataEn } from '../../src/data/adam/a2';
+import { mosesA2BookDataAr, mosesA2BookDataEn } from '../../src/data/moses/a2';
 import {
   validateLearningBookStructure,
   type LearningBookStructureConfig,
@@ -13,9 +14,20 @@ const adamA2Config: LearningBookStructureConfig = {
   finalChallengePageId: 16,
 };
 
+const mosesA2Config: LearningBookStructureConfig = {
+  storyIds: Array.from({ length: 16 }, (_, index) => index + 1),
+  knowledgeCheckPageId: 17,
+  vocabularyPageId: 18,
+  reviewPageId: 21,
+  glossaryPageIds: [19, 20],
+  finalChallengePageId: 22,
+};
+
 const migratedBooks = [
   { label: 'Adam A2 EN', book: adamA2BookDataEn, config: adamA2Config },
   { label: 'Adam A2 AR', book: adamA2BookDataAr, config: adamA2Config },
+  { label: 'Moses A2 EN', book: mosesA2BookDataEn, config: mosesA2Config },
+  { label: 'Moses A2 AR', book: mosesA2BookDataAr, config: mosesA2Config },
 ];
 
 let failed = false;
