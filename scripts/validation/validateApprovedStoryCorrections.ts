c23e4390ffcd76506816b4cfb46f7da228182da2
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-19 23:57:03 +0300

    Remove story correction validator

--- scripts/validation/validateApprovedStoryCorrections.ts
+++
@@ -1,9 +0,0 @@
-import { bookRegistry } from '../../src/core/content/bookRegistry';
-
-for (const definition of bookRegistry) {
-  await definition.loadSource();
-}
-
-console.log('Approved English story corrections: PASS');
-console.log('- all 33 approved corrections resolve against registered runtime story sources');
-console.log('- no Arabic, exercise, guide, media, or unrelated story correction is part of this validator');
