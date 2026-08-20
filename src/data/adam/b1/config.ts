e9ed42c1982b12a96b49b81c12826501cc4a6c2d
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-20 13:30:28 +0300

    Remove obsolete Adam B1 blueprint config

--- src/data/adam/b1/config.ts
+++
@@ -1,9 +0,0 @@
-import type { B1BlueprintConfig } from '../../b1BlueprintSystem';
-
-export const adamB1BlueprintConfig: B1BlueprintConfig = {
-  storyIds: Array.from({ length: 12 }, (_, index) => index + 1),
-  knowledgeCheckPageId: 13,
-  reviewPageId: 14,
-  glossaryPageIds: [15, 16],
-  finalChallengePageId: 17,
-};
