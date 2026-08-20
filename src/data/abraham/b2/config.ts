773accd57d4ad641917d6c057fae7a75ee70e09d
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-20 03:20:01 +0300

    qa: remove Abraham B2 legacy config

--- src/data/abraham/b2/config.ts
+++
@@ -1,9 +0,0 @@
-import type { B2BlueprintConfig } from '../../b2BlueprintSystem';
-
-export const abrahamB2BlueprintConfig: B2BlueprintConfig = {
-  storyIds: Array.from({ length: 35 }, (_, index) => index + 1),
-  knowledgeCheckPageId: 36,
-  reviewPageId: 37,
-  glossaryPageIds: [38, 39],
-  finalChallengePageId: 40,
-};
