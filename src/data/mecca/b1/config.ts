8bec90d92a768195cc0cee3067e6a7f3044cad18
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-20 08:24:29 +0300

    qa: remove obsolete Mecca B1 config

--- src/data/mecca/b1/config.ts
+++
@@ -1,9 +0,0 @@
-import type { B1BlueprintConfig } from '../../b1BlueprintSystem';
-
-export const meccaB1BlueprintConfig: B1BlueprintConfig = {
-  storyIds: Array.from({ length: 15 }, (_, index) => index + 1),
-  knowledgeCheckPageId: 16,
-  reviewPageId: 17,
-  glossaryPageIds: [18, 19],
-  finalChallengePageId: 20,
-};
