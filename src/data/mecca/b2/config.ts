34d0e4a3ba837af098664fa3f8a5d6f531c0da75
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-20 09:25:51 +0300

    Remove obsolete Mecca B2 blueprint config

--- src/data/mecca/b2/config.ts
+++
@@ -1,9 +0,0 @@
-import type { B2BlueprintConfig } from '../../b2BlueprintSystem';
-
-export const meccaB2BlueprintConfig: B2BlueprintConfig = {
-  storyIds: Array.from({ length: 17 }, (_, index) => index + 1),
-  knowledgeCheckPageId: 18,
-  reviewPageId: 19,
-  glossaryPageIds: [20, 21],
-  finalChallengePageId: 22,
-};
