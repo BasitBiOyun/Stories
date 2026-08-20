b1bf2fedb1583c32e63988bb8911c068b2467aca
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-20 11:27:23 +0300

    qa(yunus-emre-b1): remove obsolete config layer

--- src/data/yunusEmre/b1/config.ts
+++
@@ -1,8 +0,0 @@
-export const yunusEmreB1BlueprintConfig = {
-  storyIds: Array.from({ length: 13 }, (_, index) => index + 1),
-  knowledgeCheckPageId: 14,
-  vocabularyPageId: 15,
-  glossaryPageIds: [16, 17] as [number, number],
-  reviewPageId: 18,
-  finalChallengePageId: 19,
-};
