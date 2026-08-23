0d923659f8f419390d69eaf49b1c0678b8369b39
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-23 15:29:02 +0300

    Wire Mecca A2 Chapter 3 language focus

--- src/data/mecca/a2/index.ts
+++ src/data/mecca/a2/index.ts
@@ -9,6 +9,7 @@ import {
 } from './en/exercises';
 import { meccaA2KnowledgeCheckExercises } from './en/knowledgeCheck';
 import { meccaA2LanguageFocusExercises } from './en/languageFocus';
+import { meccaA2LanguageFocusExercisesPart2 } from './en/languageFocusPart2';
 import {
   meccaA2FinalChallengeExercisesAr,
   meccaA2FinalReviewExercisesAr,
@@ -17,6 +18,7 @@ import {
 } from './ar/exercises';
 import { meccaA2KnowledgeCheckExercisesAr } from './ar/knowledgeCheck';
 import { meccaA2LanguageFocusExercisesAr } from './ar/languageFocus';
+import { meccaA2LanguageFocusExercisesArPart2 } from './ar/languageFocusPart2';
 import { meccaA2TeacherGuide, meccaA2TeacherGuideMetadata } from './en/teacherGuide';
 import { meccaA2TeacherGuideAr, meccaA2TeacherGuideMetadataAr } from './ar/teacherGuide';
 import { meccaA2SelfStudyGuide, meccaA2StudentGuideMetadata } from './en/selfStudyGuide';
@@ -26,9 +28,10 @@ const STORY_IDS = new Set(Array.from({ length: 13 }, (_, index) => index + 1));
 
 const buildEnglishPages = (): PageData[] => meccaA2PagesEn.map(page => {
   if (STORY_IDS.has(page.id)) {
+    const languageFocus = meccaA2LanguageFocusExercisesPart2[page.id] ?? meccaA2LanguageFocusExercises[page.id] ?? [];
     return {
       ...page,
-      exercises: [meccaA2QuickChallenges[page.id], ...(meccaA2LanguageFocusExercises[page.id] ?? [])],
+      exercises: [meccaA2QuickChallenges[page.id], ...languageFocus],
     };
   }
   if (page.id === 14) return { ...page, exercises: meccaA2KnowledgeCheckExercises };
@@ -40,9 +43,10 @@ const buildEnglishPages = (): PageData[] => meccaA2PagesEn.map(page => {
 
 const buildArabicPages = (): PageData[] => meccaA2PagesAr.map(page => {
   if (STORY_IDS.has(page.id)) {
+    const languageFocus = meccaA2LanguageFocusExercisesArPart2[page.id] ?? meccaA2LanguageFocusExercisesAr[page.id] ?? [];
     return {
       ...page,
-      exercises: [meccaA2QuickChallengesAr[page.id], ...(meccaA2LanguageFocusExercisesAr[page.id] ?? [])],
+      exercises: [meccaA2QuickChallengesAr[page.id], ...languageFocus],
     };
   }
   if (page.id === 14) return { ...page, exercises: meccaA2KnowledgeCheckExercisesAr };
