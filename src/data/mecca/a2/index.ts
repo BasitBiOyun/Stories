4bb57a086ce95760c318deb7fe97e271538f00d0
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-23 16:30:48 +0300

    Wire Mecca A2 Chapter 4 Language Focus

--- src/data/mecca/a2/index.ts
+++ src/data/mecca/a2/index.ts
@@ -10,6 +10,7 @@ import {
 import { meccaA2KnowledgeCheckExercises } from './en/knowledgeCheck';
 import { meccaA2LanguageFocusExercises } from './en/languageFocus';
 import { meccaA2LanguageFocusExercisesPart2 } from './en/languageFocusPart2';
+import { meccaA2LanguageFocusExercisesPart3 } from './en/languageFocusPart3';
 import {
   meccaA2FinalChallengeExercisesAr,
   meccaA2FinalReviewExercisesAr,
@@ -19,6 +20,7 @@ import {
 import { meccaA2KnowledgeCheckExercisesAr } from './ar/knowledgeCheck';
 import { meccaA2LanguageFocusExercisesAr } from './ar/languageFocus';
 import { meccaA2LanguageFocusExercisesArPart2 } from './ar/languageFocusPart2';
+import { meccaA2LanguageFocusExercisesArPart3 } from './ar/languageFocusPart3';
 import { meccaA2TeacherGuide, meccaA2TeacherGuideMetadata } from './en/teacherGuide';
 import { meccaA2TeacherGuideAr, meccaA2TeacherGuideMetadataAr } from './ar/teacherGuide';
 import { meccaA2SelfStudyGuide, meccaA2StudentGuideMetadata } from './en/selfStudyGuide';
@@ -28,7 +30,7 @@ const STORY_IDS = new Set(Array.from({ length: 13 }, (_, index) => index + 1));
 
 const buildEnglishPages = (): PageData[] => meccaA2PagesEn.map(page => {
   if (STORY_IDS.has(page.id)) {
-    const languageFocus = meccaA2LanguageFocusExercisesPart2[page.id] ?? meccaA2LanguageFocusExercises[page.id] ?? [];
+    const languageFocus = meccaA2LanguageFocusExercisesPart3[page.id] ?? meccaA2LanguageFocusExercisesPart2[page.id] ?? meccaA2LanguageFocusExercises[page.id] ?? [];
     return {
       ...page,
       exercises: [meccaA2QuickChallenges[page.id], ...languageFocus],
@@ -43,7 +45,7 @@ const buildEnglishPages = (): PageData[] => meccaA2PagesEn.map(page => {
 
 const buildArabicPages = (): PageData[] => meccaA2PagesAr.map(page => {
   if (STORY_IDS.has(page.id)) {
-    const languageFocus = meccaA2LanguageFocusExercisesArPart2[page.id] ?? meccaA2LanguageFocusExercisesAr[page.id] ?? [];
+    const languageFocus = meccaA2LanguageFocusExercisesArPart3[page.id] ?? meccaA2LanguageFocusExercisesArPart2[page.id] ?? meccaA2LanguageFocusExercisesAr[page.id] ?? [];
     return {
       ...page,
       exercises: [meccaA2QuickChallengesAr[page.id], ...languageFocus],
