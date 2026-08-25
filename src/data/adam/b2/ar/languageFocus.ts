333014c606cf0cad4f63ddee7a958c45212a31c0
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-25 22:33:15 +0300

    Wire Adam B2 Chapter 8 Arabic Language Focus

--- src/data/adam/b2/ar/languageFocus.ts
+++ src/data/adam/b2/ar/languageFocus.ts
@@ -3,6 +3,7 @@ import { adamB2LanguageFocusExercisesAr as adamB2LanguageFocusBaseCh1to4Ar } fro
 import { adamB2LanguageFocusChapter5Ar } from './languageFocusChapter5';
 import { adamB2LanguageFocusChapter6Ar } from './languageFocusChapter6';
 import { adamB2LanguageFocusChapter7Ar } from './languageFocusChapter7';
+import { adamB2LanguageFocusChapter8Ar } from './languageFocusChapter8';
 
 /** التجميع النشط للتركيز اللغوي العربي في آدم B2 من مجموعات الفصول المصاغة يدويا. */
 export const adamB2LanguageFocusExercisesAr: Record<number, Exercise[]> = {
@@ -10,4 +11,5 @@ export const adamB2LanguageFocusExercisesAr: Record<number, Exercise[]> = {
   ...adamB2LanguageFocusChapter5Ar,
   ...adamB2LanguageFocusChapter6Ar,
   ...adamB2LanguageFocusChapter7Ar,
+  ...adamB2LanguageFocusChapter8Ar,
 };
