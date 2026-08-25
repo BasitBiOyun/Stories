99db3f516b84e0944cc6d543d3eacab7b6343284
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-25 19:01:09 +0300

    Wire Adam B1 Chapter 5 Language Focus

--- src/data/adam/b1/index.ts
+++ src/data/adam/b1/index.ts
@@ -18,9 +18,11 @@ import {
 import { adamB1LanguageFocusExercises } from './en/languageFocus';
 import { adamB1LanguageFocusExercisesPart2 } from './en/languageFocusPart2';
 import { adamB1LanguageFocusExercisesPart3 } from './en/languageFocusPart3';
+import { adamB1LanguageFocusExercisesPart4 } from './en/languageFocusPart4';
 import { adamB1LanguageFocusExercisesAr } from './ar/languageFocus';
 import { adamB1LanguageFocusExercisesArPart2 } from './ar/languageFocusPart2';
 import { adamB1LanguageFocusExercisesArPart3 } from './ar/languageFocusPart3';
+import { adamB1LanguageFocusExercisesArPart4 } from './ar/languageFocusPart4';
 import { adamB1TeacherGuide, adamB1TeacherGuideMetadata } from './en/teacherGuide';
 import { adamB1SelfStudyGuide, adamB1StudentGuideMetadata } from './en/selfStudyGuide';
 import { adamB1TeacherGuideAr, adamB1TeacherGuideMetadataAr } from './ar/teacherGuide';
@@ -32,12 +34,14 @@ const englishLanguageFocus = {
   ...adamB1LanguageFocusExercises,
   ...adamB1LanguageFocusExercisesPart2,
   ...adamB1LanguageFocusExercisesPart3,
+  ...adamB1LanguageFocusExercisesPart4,
 };
 
 const arabicLanguageFocus = {
   ...adamB1LanguageFocusExercisesAr,
   ...adamB1LanguageFocusExercisesArPart2,
   ...adamB1LanguageFocusExercisesArPart3,
+  ...adamB1LanguageFocusExercisesArPart4,
 };
 
 const buildPages = (
