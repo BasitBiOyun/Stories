60c3a8eb5ee05bde268f447748ad996678f166cd
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-26 10:02:59 +0300

    Wire Abraham B1 Chapter 5 Language Focus

--- src/data/abraham/b1/index.ts
+++ src/data/abraham/b1/index.ts
@@ -21,6 +21,8 @@ import { abrahamB1LanguageFocusChapter3 } from './en/languageFocusChapter3';
 import { abrahamB1LanguageFocusChapter3Ar } from './ar/languageFocusChapter3';
 import { abrahamB1LanguageFocusChapter4 } from './en/languageFocusChapter4';
 import { abrahamB1LanguageFocusChapter4Ar } from './ar/languageFocusChapter4';
+import { abrahamB1LanguageFocusChapter5 } from './en/languageFocusChapter5';
+import { abrahamB1LanguageFocusChapter5Ar } from './ar/languageFocusChapter5';
 import { abrahamB1TeacherGuideEn, abrahamB1TeacherGuideMetadata } from './en/teacherGuide';
 import { abrahamB1TeacherGuideAr, abrahamB1TeacherGuideMetadataAr } from './ar/teacherGuide';
 import { abrahamB1SelfStudyGuideEn } from './en/selfstudyGuide';
@@ -64,7 +66,7 @@ const buildEnglishPages = (): PageData[] => englishSourcePages.map(page => {
       ...page,
       exercises: [
         abrahamB1QuickChallenges[page.id],
-        ...(abrahamB1LanguageFocusChapter4[page.id] ?? abrahamB1LanguageFocusChapter3[page.id] ?? abrahamB1LanguageFocusExercises[page.id] ?? []),
+        ...(abrahamB1LanguageFocusChapter5[page.id] ?? abrahamB1LanguageFocusChapter4[page.id] ?? abrahamB1LanguageFocusChapter3[page.id] ?? abrahamB1LanguageFocusExercises[page.id] ?? []),
       ].filter(Boolean),
     };
   }
@@ -81,7 +83,7 @@ const buildArabicPages = (): PageData[] => arabicSourcePages.map(page => {
       ...page,
       exercises: [
         abrahamB1QuickChallengesAr[page.id],
-        ...(abrahamB1LanguageFocusChapter4Ar[page.id] ?? abrahamB1LanguageFocusChapter3Ar[page.id] ?? abrahamB1LanguageFocusExercisesAr[page.id] ?? []),
+        ...(abrahamB1LanguageFocusChapter5Ar[page.id] ?? abrahamB1LanguageFocusChapter4Ar[page.id] ?? abrahamB1LanguageFocusChapter3Ar[page.id] ?? abrahamB1LanguageFocusExercisesAr[page.id] ?? []),
       ].filter(Boolean),
     };
   }
