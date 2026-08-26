12a7a4bd11ed7d379b92dced72e6620fbb715656
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-26 11:34:07 +0300

    Add Abraham B2 English Language Focus aggregator

---
+++ src/data/abraham/b2/en/languageFocus.ts
@@ -0,0 +1,7 @@
+import type { Exercise } from '../../../../types';
+import { abrahamB2LanguageFocusChapter1 } from './languageFocusChapter1';
+
+/** Active Abraham B2 English Language Focus assembled from manually authored chapter sets. */
+export const abrahamB2LanguageFocusExercises: Record<number, Exercise[]> = {
+  ...abrahamB2LanguageFocusChapter1,
+};
