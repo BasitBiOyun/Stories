6cf833c3cdbfedbce0ec955848ef22a3aa59c45e
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-26 05:33:14 +0300

    Wire Adam B2 Chapter 15 English Language Focus

--- src/data/adam/b2/en/languageFocus.ts
+++ src/data/adam/b2/en/languageFocus.ts
@@ -10,6 +10,7 @@ import { adamB2LanguageFocusChapter11 } from './languageFocusChapter11';
 import { adamB2LanguageFocusChapter12 } from './languageFocusChapter12';
 import { adamB2LanguageFocusChapter13 } from './languageFocusChapter13';
 import { adamB2LanguageFocusChapter14 } from './languageFocusChapter14';
+import { adamB2LanguageFocusChapter15 } from './languageFocusChapter15';
 
 /** Active Adam B2 English Language Focus assembled from manually authored chapter sets. */
 export const adamB2LanguageFocusExercises: Record<number, Exercise[]> = {
@@ -24,4 +25,5 @@ export const adamB2LanguageFocusExercises: Record<number, Exercise[]> = {
   ...adamB2LanguageFocusChapter12,
   ...adamB2LanguageFocusChapter13,
   ...adamB2LanguageFocusChapter14,
+  ...adamB2LanguageFocusChapter15,
 };
