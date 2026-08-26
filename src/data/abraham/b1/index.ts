6332a89b9e3d55b133cd21fb66a5104f796bae0a
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-26 15:03:43 +0300

    Wire Abraham B1 Chapter 10 Language Focus

--- src/data/abraham/b1/index.ts
+++ src/data/abraham/b1/index.ts
@@ -31,6 +31,8 @@ import { abrahamB1LanguageFocusChapter8 } from './en/languageFocusChapter8';
 import { abrahamB1LanguageFocusChapter8Ar } from './ar/languageFocusChapter8';
 import { abrahamB1LanguageFocusChapter9 } from './en/languageFocusChapter9';
 import { abrahamB1LanguageFocusChapter9Ar } from './ar/languageFocusChapter9';
+import { abrahamB1LanguageFocusChapter10 } from './en/languageFocusChapter10';
+import { abrahamB1LanguageFocusChapter10Ar } from './ar/languageFocusChapter10';
 import { abrahamB1TeacherGuideEn, abrahamB1TeacherGuideMetadata } from './en/teacherGuide';
 import { abrahamB1TeacherGuideAr, abrahamB1TeacherGuideMetadataAr } from './ar/teacherGuide';
 import { abrahamB1SelfStudyGuideEn } from './en/selfstudyGuide';
@@ -70,12 +72,11 @@ const arabicSourcePages = prepareMediaFallbacks(abrahamB1PagesAr);
 
 const buildEnglishPages = (): PageData[] => englishSourcePages.map(page => {
   if (STORY_IDS.has(page.id)) {
+    const languageFocusExercises = abrahamB1LanguageFocusChapter10[page.id] ?? abrahamB1LanguageFocusChapter9[page.id] ?? abrahamB1LanguageFocusChapter8[page.id] ?? abrahamB1LanguageFocusChapter7[page.id] ?? abrahamB1LanguageFocusChapter6[page.id] ?? abrahamB1LanguageFocusChapter5[page.id] ?? abrahamB1LanguageFocusChapter4[page.id] ?? abrahamB1LanguageFocusChapter3[page.id] ?? abrahamB1LanguageFocusExercises[page.id];
     return {
       ...page,
-      exercises: [
-        abrahamB1QuickChallenges[page.id],
-        ...(abrahamB1LanguageFocusChapter9[page.id] ?? abrahamB1LanguageFocusChapter8[page.id] ?? abrahamB1LanguageFocusChapter7[page.id] ?? abrahamB1LanguageFocusChapter6[page.id] ?? abrahamB1LanguageFocusChapter5[page.id] ?? abrahamB1LanguageFocusChapter4[page.id] ?? abrahamB1LanguageFocusChapter3[page.id] ?? abrahamB1LanguageFocusExercises[page.id] ?? []),
-      ].filter(Boolean),
+      exercises: abrahamB1QuickChallenges[page.id] ? [abrahamB1QuickChallenges[page.id]] : [],
+      ...(languageFocusExercises ? { languageFocusExercises } : {}),
     };
   }
   if (page.id === 14) return { ...page, exercises: abrahamB1KnowledgeCheckExercises };
@@ -87,12 +88,11 @@ const buildEnglishPages = (): PageData[] => englishSourcePages.map(page => {
 
 const buildArabicPages = (): PageData[] => arabicSourcePages.map(page => {
   if (STORY_IDS.has(page.id)) {
+    const languageFocusExercises = abrahamB1LanguageFocusChapter10Ar[page.id] ?? abrahamB1LanguageFocusChapter9Ar[page.id] ?? abrahamB1LanguageFocusChapter8Ar[page.id] ?? abrahamB1LanguageFocusChapter7Ar[page.id] ?? abrahamB1LanguageFocusChapter6Ar[page.id] ?? abrahamB1LanguageFocusChapter5Ar[page.id] ?? abrahamB1LanguageFocusChapter4Ar[page.id] ?? abrahamB1LanguageFocusChapter3Ar[page.id] ?? abrahamB1LanguageFocusExercisesAr[page.id];
     return {
       ...page,
-      exercises: [
-        abrahamB1QuickChallengesAr[page.id],
-        ...(abrahamB1LanguageFocusChapter9Ar[page.id] ?? abrahamB1LanguageFocusChapter8Ar[page.id] ?? abrahamB1LanguageFocusChapter7Ar[page.id] ?? abrahamB1LanguageFocusChapter6Ar[page.id] ?? abrahamB1LanguageFocusChapter5Ar[page.id] ?? abrahamB1LanguageFocusChapter4Ar[page.id] ?? abrahamB1LanguageFocusChapter3Ar[page.id] ?? abrahamB1LanguageFocusExercisesAr[page.id] ?? []),
-      ].filter(Boolean),
+      exercises: abrahamB1QuickChallengesAr[page.id] ? [abrahamB1QuickChallengesAr[page.id]] : [],
+      ...(languageFocusExercises ? { languageFocusExercises } : {}),
     };
   }
   if (page.id === 14) return { ...page, exercises: abrahamB1KnowledgeCheckExercisesAr };
