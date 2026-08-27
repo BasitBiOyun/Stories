e0df61af4bf4733fd02749a20a758e2cada7c917
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-28 00:04:52 +0300

    Wire Mecca B1 Chapter 11 Language Focus

--- src/data/mecca/b1/index.ts
+++ src/data/mecca/b1/index.ts
@@ -15,6 +15,7 @@ import { meccaB1LanguageFocusChapter7 } from './en/languageFocusChapter7';
 import { meccaB1LanguageFocusChapter8 } from './en/languageFocusChapter8';
 import { meccaB1LanguageFocusChapter9 } from './en/languageFocusChapter9';
 import { meccaB1LanguageFocusChapter10 } from './en/languageFocusChapter10';
+import { meccaB1LanguageFocusChapter11 } from './en/languageFocusChapter11';
 import { meccaB1KnowledgeCheckExercises } from './en/knowledgeCheck';
 import { meccaB1TeacherGuide, meccaB1TeacherGuideMetadata } from './en/teacherGuide';
 import { meccaB1SelfStudyGuide, meccaB1StudentGuideMetadata } from './en/selfStudyGuide';
@@ -34,6 +35,7 @@ import { meccaB1LanguageFocusChapter7Ar } from './ar/languageFocusChapter7';
 import { meccaB1LanguageFocusChapter8Ar } from './ar/languageFocusChapter8';
 import { meccaB1LanguageFocusChapter9Ar } from './ar/languageFocusChapter9';
 import { meccaB1LanguageFocusChapter10Ar } from './ar/languageFocusChapter10';
+import { meccaB1LanguageFocusChapter11Ar } from './ar/languageFocusChapter11';
 import { meccaB1KnowledgeCheckExercisesAr } from './ar/knowledgeCheck';
 import { meccaB1TeacherGuideAr, meccaB1TeacherGuideMetadataAr } from './ar/teacherGuide';
 import { meccaB1SelfStudyGuideAr, meccaB1StudentGuideMetadataAr } from './ar/selfStudyGuide';
@@ -48,6 +50,7 @@ const englishLanguageFocus = {
   ...meccaB1LanguageFocusChapter8,
   ...meccaB1LanguageFocusChapter9,
   ...meccaB1LanguageFocusChapter10,
+  ...meccaB1LanguageFocusChapter11,
 };
 
 const arabicLanguageFocus = {
@@ -60,6 +63,7 @@ const arabicLanguageFocus = {
   ...meccaB1LanguageFocusChapter8Ar,
   ...meccaB1LanguageFocusChapter9Ar,
   ...meccaB1LanguageFocusChapter10Ar,
+  ...meccaB1LanguageFocusChapter11Ar,
 };
 
 const attachLearning = (
