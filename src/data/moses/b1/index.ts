c3b13dceab42a173926682841016900fb63f9683
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-27 09:00:24 +0300

    Wire Moses B1 Chapter 12 Language Focus

--- src/data/moses/b1/index.ts
+++ src/data/moses/b1/index.ts
@@ -25,6 +25,7 @@ import { mosesB1LanguageFocusChapter8 } from './en/languageFocusChapter8';
 import { mosesB1LanguageFocusChapter9 } from './en/languageFocusChapter9';
 import { mosesB1LanguageFocusChapter10 } from './en/languageFocusChapter10';
 import { mosesB1LanguageFocusChapter11 } from './en/languageFocusChapter11';
+import { mosesB1LanguageFocusChapter12 } from './en/languageFocusChapter12';
 import { mosesB1LanguageFocusExercisesAr } from './ar/languageFocus';
 import { mosesB1LanguageFocusChapter3Ar } from './ar/languageFocusChapter3';
 import { mosesB1LanguageFocusChapter4Ar } from './ar/languageFocusChapter4';
@@ -35,6 +36,7 @@ import { mosesB1LanguageFocusChapter8Ar } from './ar/languageFocusChapter8';
 import { mosesB1LanguageFocusChapter9Ar } from './ar/languageFocusChapter9';
 import { mosesB1LanguageFocusChapter10Ar } from './ar/languageFocusChapter10';
 import { mosesB1LanguageFocusChapter11Ar } from './ar/languageFocusChapter11';
+import { mosesB1LanguageFocusChapter12Ar } from './ar/languageFocusChapter12';
 import { mosesB1TeacherGuide, mosesB1TeacherGuideMetadata } from './en/teacherGuide';
 import { mosesB1SelfStudyGuide, mosesB1StudentGuideMetadata } from './en/selfStudyGuide';
 import { mosesB1TeacherGuideAr, mosesB1TeacherGuideMetadataAr } from './ar/teacherGuide';
@@ -53,6 +55,7 @@ const englishLanguageFocus = {
   ...mosesB1LanguageFocusChapter9,
   ...mosesB1LanguageFocusChapter10,
   ...mosesB1LanguageFocusChapter11,
+  ...mosesB1LanguageFocusChapter12,
 };
 
 const arabicLanguageFocus = {
@@ -66,6 +69,7 @@ const arabicLanguageFocus = {
   ...mosesB1LanguageFocusChapter9Ar,
   ...mosesB1LanguageFocusChapter10Ar,
   ...mosesB1LanguageFocusChapter11Ar,
+  ...mosesB1LanguageFocusChapter12Ar,
 };
 
 const buildPages = (
