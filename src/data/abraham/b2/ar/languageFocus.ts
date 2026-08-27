dc60d2a55d74a57ba15e93c147acf2a2eb0d6230
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-27 09:31:08 +0300

    Wire Abraham B2 Chapter 23 Arabic Language Focus

--- src/data/abraham/b2/ar/languageFocus.ts
+++ src/data/abraham/b2/ar/languageFocus.ts
@@ -21,6 +21,7 @@ import { abrahamB2LanguageFocusChapter19Ar } from './languageFocusChapter19';
 import { abrahamB2LanguageFocusChapter20Ar } from './languageFocusChapter20';
 import { abrahamB2LanguageFocusChapter21Ar } from './languageFocusChapter21';
 import { abrahamB2LanguageFocusChapter22Ar } from './languageFocusChapter22';
+import { abrahamB2LanguageFocusChapter23Ar } from './languageFocusChapter23';
 
 /** تمارين التركيز اللغوي النشطة في إبراهيم B2، مجمعة من مجموعات الفصول المؤلفة يدويًا. */
 export const abrahamB2LanguageFocusExercisesAr: Record<number, Exercise[]> = {
@@ -46,4 +47,5 @@ export const abrahamB2LanguageFocusExercisesAr: Record<number, Exercise[]> = {
   ...abrahamB2LanguageFocusChapter20Ar,
   ...abrahamB2LanguageFocusChapter21Ar,
   ...abrahamB2LanguageFocusChapter22Ar,
+  ...abrahamB2LanguageFocusChapter23Ar,
 };
