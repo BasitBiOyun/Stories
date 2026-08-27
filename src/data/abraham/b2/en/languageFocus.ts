d6864cd332f7ecee7031479abdacd97ef60b25d2
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-08-27 09:30:52 +0300

    Wire Abraham B2 Chapter 23 English Language Focus

--- src/data/abraham/b2/en/languageFocus.ts
+++ src/data/abraham/b2/en/languageFocus.ts
@@ -21,6 +21,7 @@ import { abrahamB2LanguageFocusChapter19 } from './languageFocusChapter19';
 import { abrahamB2LanguageFocusChapter20 } from './languageFocusChapter20';
 import { abrahamB2LanguageFocusChapter21 } from './languageFocusChapter21';
 import { abrahamB2LanguageFocusChapter22 } from './languageFocusChapter22';
+import { abrahamB2LanguageFocusChapter23 } from './languageFocusChapter23';
 
 /** Active Abraham B2 English Language Focus assembled from manually authored chapter sets. */
 export const abrahamB2LanguageFocusExercises: Record<number, Exercise[]> = {
@@ -46,4 +47,5 @@ export const abrahamB2LanguageFocusExercises: Record<number, Exercise[]> = {
   ...abrahamB2LanguageFocusChapter20,
   ...abrahamB2LanguageFocusChapter21,
   ...abrahamB2LanguageFocusChapter22,
+  ...abrahamB2LanguageFocusChapter23,
 };
