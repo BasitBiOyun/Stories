bae7f98ca49c18ece01b5f1ea6fcdeb64e4f6807
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-09-03 10:22:15 +0300

    Use complete Abraham A2 full self-study guides in preview

--- src/data/abraham/a2/index.ts
+++ src/data/abraham/a2/index.ts
@@ -9,13 +9,13 @@ import {
   abrahamA2StudentGuideMetadataFinalEn,
   abrahamA2StudentGuideSectionsFinalAr,
   abrahamA2StudentGuideSectionsFinalEn,
-  abrahamA2StudentGuideTextFinalAr,
-  abrahamA2StudentGuideTextFinalEn,
   abrahamA2TeacherGuideFinalAr,
   abrahamA2TeacherGuideFinalEn,
   abrahamA2TeacherGuideMetadataFinalAr,
   abrahamA2TeacherGuideMetadataFinalEn,
 } from './goldFinal';
+import { abrahamA2FullGuideText } from './en/fullGuideText';
+import { abrahamA2FullGuideTextAr } from './ar/fullGuideText';
 
 const abrahamA2PagesLockedEn = applyA2FinalStoryLanguageLock(abrahamA2PagesFinalEn, 'ibrahim', 'en');
 const abrahamA2PagesLockedAr = applyA2FinalStoryLanguageLock(abrahamA2PagesFinalAr, 'ibrahim', 'ar');
@@ -31,7 +31,7 @@ export const abrahamA2BookDataEn: BookData = {
   selfStudyGuide: abrahamA2SelfStudyGuideFinalEn,
   studentGuideSections: abrahamA2StudentGuideSectionsFinalEn,
   studentGuideMetadata: abrahamA2StudentGuideMetadataFinalEn,
-  studentGuideText: abrahamA2StudentGuideTextFinalEn,
+  studentGuideText: abrahamA2FullGuideText,
 };
 
 export const abrahamA2BookDataAr: BookData = {
@@ -45,7 +45,7 @@ export const abrahamA2BookDataAr: BookData = {
   selfStudyGuide: abrahamA2SelfStudyGuideFinalAr,
   studentGuideSections: abrahamA2StudentGuideSectionsFinalAr,
   studentGuideMetadata: abrahamA2StudentGuideMetadataFinalAr,
-  studentGuideText: abrahamA2StudentGuideTextFinalAr,
+  studentGuideText: abrahamA2FullGuideTextAr,
 };
 
 export const abrahamA2BookData = abrahamA2BookDataEn;
