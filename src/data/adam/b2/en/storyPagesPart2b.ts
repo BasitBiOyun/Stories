275d1988b03ebe11e29ffb9b74bf0082d8430870
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-09-04 17:42:10 +0300

    Add corrected Adam B2 English story pages 14-15

---
+++ src/data/adam/b2/en/storyPagesPart2b.ts
@@ -0,0 +1,10 @@
+import { PageData } from '../../../../types';
+
+export const adamB2StoryPagesPart2b: PageData[] = [
+  {
+    id: 14,
+    type: 'story',
+    title: 'The First Murder & Burial',
+    image: 'https://firebasestorage.googleapis.com/v0/b/gen-lang-client-0373200489.firebasestorage.app/o/adam_b2%2Fimages%2Fadam_b2_chapter14.png?alt=media&token=4399b928-a16f-4bdd-a697-b274b27d32c2',
+    audioUrl: 'https://firebasestorage.googleapis.com/v0/b/gen-lang-client-0373200489.firebasestorage.app/o/adam_b2%2Faudio%2Fadam_b2_ch14.mp3?alt=media&token=f976eee6-6b6d-4aa5-9b81-79a43cb62a61',
+    content: "This brotherly request did nothing to lessen the hatred in Qabil's heart, nor did he show fear of Allah's punishment. Even family ties were disregarded. Qabil hit his brother with a stone, killing him instantly. This was the first death and the first criminal act committed by man on Earth.\n\nSurah Maide concludes the story with Qabil’s tragic end:\n\n“At last his evil soul drove him (Qabil) to the murder of his brother, and he killed him, by which he himself became one of the losers.” (Maide: 30)\n\nWhen Habil had not appeared for some time, Adam (pbuh) began to search for him but found no trace of his beloved son. He asked Qabil about Habil's location. Qabil arrogantly replied that he was not his brother's keeper nor his protector. From these words, his father understood that Habil was dead, and Adam (pbuh) was filled with grief. Meanwhile, Qabil did not know what to do with his brother's corpse. He carried it on his back, wandering from place to place trying to
