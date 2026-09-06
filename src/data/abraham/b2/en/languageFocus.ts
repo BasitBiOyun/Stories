401bf666d131fbab993eb703bf0ceb484510661f
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-09-06 20:19:18 +0300

    Wire Abraham B2 EN language focus block 1-12

--- src/data/abraham/b2/en/languageFocus.ts
+++ src/data/abraham/b2/en/languageFocus.ts
@@ -1,16 +1,5 @@
 import type { Exercise } from '../../../../types';
-import { abrahamB2LanguageFocusChapter1 } from './languageFocusChapter1';
-import { abrahamB2LanguageFocusChapter2 } from './languageFocusChapter2';
-import { abrahamB2LanguageFocusChapter3 } from './languageFocusChapter3';
-import { abrahamB2LanguageFocusChapter4 } from './languageFocusChapter4';
-import { abrahamB2LanguageFocusChapter5 } from './languageFocusChapter5';
-import { abrahamB2LanguageFocusChapter6 } from './languageFocusChapter6';
-import { abrahamB2LanguageFocusChapter7 } from './languageFocusChapter7';
-import { abrahamB2LanguageFocusChapter8 } from './languageFocusChapter8';
-import { abrahamB2LanguageFocusChapter9 } from './languageFocusChapter9';
-import { abrahamB2LanguageFocusChapter10 } from './languageFocusChapter10';
-import { abrahamB2LanguageFocusChapter11 } from './languageFocusChapter11';
-import { abrahamB2LanguageFocusChapter12 } from './languageFocusChapter12';
+import { abrahamB2LanguageFocusPart1 } from './languageFocus2';
 import { abrahamB2LanguageFocusChapter13 } from './languageFocusChapter13';
 import { abrahamB2LanguageFocusChapter14 } from './languageFocusChapter14';
 import { abrahamB2LanguageFocusChapter15 } from './languageFocusChapter15';
@@ -37,18 +26,7 @@ import { abrahamB2LanguageFocusChapter35 } from './languageFocusChapter35';
 
 /** Active Abraham B2 English Language Focus assembled from manually authored chapter sets. */
 export const abrahamB2LanguageFocusExercises: Record<number, Exercise[]> = {
-  ...abrahamB2LanguageFocusChapter1,
-  ...abrahamB2LanguageFocusChapter2,
-  ...abrahamB2LanguageFocusChapter3,
-  ...abrahamB2LanguageFocusChapter4,
-  ...abrahamB2LanguageFocusChapter5,
-  ...abrahamB2LanguageFocusChapter6,
-  ...abrahamB2LanguageFocusChapter7,
-  ...abrahamB2LanguageFocusChapter8,
-  ...abrahamB2LanguageFocusChapter9,
-  ...abrahamB2LanguageFocusChapter10,
-  ...abrahamB2LanguageFocusChapter11,
-  ...abrahamB2LanguageFocusChapter12,
+  ...abrahamB2LanguageFocusPart1,
   ...abrahamB2LanguageFocusChapter13,
   ...abrahamB2LanguageFocusChapter14,
   ...abrahamB2LanguageFocusChapter15,
