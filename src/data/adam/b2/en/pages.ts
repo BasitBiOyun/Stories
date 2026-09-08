4dedcc7420488cfe96ad2930ae58c9b6f12ec6c8
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-09-08 16:23:55 +0300

    Add Adam B2 English chapter 14 first half

--- src/data/adam/b2/en/pages.ts
+++ src/data/adam/b2/en/pages.ts
@@ -4,7 +4,12 @@ const storyPages: PageData[] = [
 //__EN_5_8__
 //__EN_9_12__
 //__EN_13__
-//__EN_14_A__
+S(14, 'The First Murder & Burial', `This brotherly request did nothing to lessen the hatred in Qabil's heart, nor did he show fear of Allah's punishment. Even familial considerations were given up. Qabil hit his brother with a stone, killing him instantly. This was the first death and the first criminal act committed by man on Earth.
+
+Surah Maide concludes the story with Qabil’s tragic end:
+
+“At last his evil soul drove him (Qabil) to the murder of his brother, and he killed him, by which he himself became one of the losers.” (Maide: 30)
+
 //__EN_14_B__
 //__EN_15_A__
 //__EN_15_B__
