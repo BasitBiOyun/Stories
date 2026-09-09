9ada2826fc3b5df5bfe1f880eb68afed5b821faa
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-09-09 15:02:26 +0300

    Load Arabic UI typography polish

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './loading.css';
 import './guideTheme.css';
 import './pdfLocked.css';
 import './layoutFixes.css';
+import './arabicTypography.css';
 import './lib/pdfDownloadLock';
 import { LanguageProvider } from './contexts/LanguageContext';
 import { FullscreenButton } from './components/ui/FullscreenButton';
