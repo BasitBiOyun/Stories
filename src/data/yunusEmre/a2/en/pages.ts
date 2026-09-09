9627002b0d405c16f2633c8cf19e561539673e2f
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-09-09 11:10:27 +0300

    Fix clear English errors in Yunus Emre A2 story

--- src/data/yunusEmre/a2/en/pages.ts
+++ src/data/yunusEmre/a2/en/pages.ts
@@ -8,7 +8,7 @@ export const yunusA2Pages: PageData[] = [
     audioUrl: 'https://firebasestorage.googleapis.com/v0/b/gen-lang-client-0373200489.firebasestorage.app/o/yunus%2Fa2%2Faudio%2F00_Chapter_1.mp3?alt=media&token=2c220cf9-a3d3-4563-bbab-c15c66f575f3',
     content: `Yunus Emre was one of the first great Anatolian Turkish poets. He was born in 1240 in Anatolia. People could easily understand his poems because he wrote and said them in simple Turkish. He helped improve the Turkish language. His poems have important moral lessons. These lessons are still very important for Turkish culture today. He also spoke of the greatness and oneness of Allah, love for Allah and Prophet Muhammad (pbuh).
 
-At that time, schools had the name of madrasa. After he completed his madrasa education, he followed the way of the dervishes and became a dervish. Dervishes practised to be a good person with moral values under the guidance of a teacher at dervish houses.
+At that time, schools were called madrasas. After he completed his madrasa education, he followed the way of the dervishes and became a dervish. Dervishes trained to be good people with moral values under the guidance of a teacher at dervish houses.
 
 Yunus Emre became a dervish pupil of Taptuk Emre. He traveled to many cities in Anatolia, as well as Syria and Azerbaijan. He lived in the same period as Mevlana. Mevlana died in 1273 and later Yunus passed away in 1320. Yunus was 33 or 34 years old when Mevlana died.`,
     vocabulary: [
@@ -84,7 +84,7 @@ Yunus Emre was one of these dervishes. He received his dervish training at the s
 
 This was also the case for Yunus. He told his master, “I will do whatever service you ask of me.” Taptuk then assigned Yunus to the wood-cutting duties at the dervish house. Some works look ordinary at first glance. The primary goal is to break the ego and give up bad behaviours.
 
-There was a mountain behind Taptuk’s dervish house. Taptuk asked Yunus to collect wood from the mountain. Every day, Yunus carried wood on his back from the mountain. But he never cut or brought green or crooked wood. He always said: “Crooked wood cannot go into the dervish house”.`,
+There was a mountain behind Taptuk’s dervish house. Taptuk asked Yunus to collect wood from the mountain. Every day, Yunus carried wood on his back from the mountain. But he never cut or brought green or crooked wood. He always said, “Crooked wood cannot go into the dervish house.”`,
     vocabulary: [
       { word: 'humble', definition: 'Not proud; respectful and modest.' },
       { word: 'service', definition: 'Work done to help others.' },
@@ -104,7 +104,7 @@ There was a mountain behind Taptuk’s dervish house. Taptuk asked Yunus to coll
 
 The wood is Yunus’s ego and it looks like it needs to be corrected. It looks like Yunus is working with wood to fix crooked pieces. But in fact, he is training and fixing his own heart and ego. Every time he uses the axe, he fixes a bad part of himself. This service taught him how to have a good heart and do the right thing.
 
-Yunus served his teacher Taptuk Emre in this way for exactly forty years. Taptuk Emre noticed that Yunus never carried crooked wood to the house. One day he asked the following question to Yunus:
+Yunus served his teacher Taptuk Emre in this way for exactly forty years. Taptuk Emre noticed that Yunus never carried crooked wood to the house. One day he asked Yunus the following question:
 
 "My Yunus, is there no crooked wood in the mountains? Why do you always bring straight pieces?"`,
     vocabulary: [
@@ -142,7 +142,7 @@ He first learned to be alone with Allah in the mountains. The mountains are quie
     id: 7, type: 'story', title: 'A Single Daisy',
     image: 'https://firebasestorage.googleapis.com/v0/b/gen-lang-client-0373200489.firebasestorage.app/o/yunus%2Fa2%2Fimages%2Fyunus_a2_ch7-clean.png?alt=media&token=a2b10e28-c657-4f96-a5a8-b70f6aedd6cd',
     audioUrl: 'https://firebasestorage.googleapis.com/v0/b/gen-lang-client-0373200489.firebasestorage.app/o/yunus%2Fa2%2Faudio%2F06_Chapter_7_A_Single_Daisy.mp3?alt=media&token=db885702-c119-40e8-ae16-a635de955363',
-    content: `One day, Taptuk Emre said to his students, “Today, all of you go up the mountain and bring me flowers. I will give a gift to whoever prepares the most beautiful bunch of flowers. All the dervishes went out into the fields. They picked flowers and ran back to their teacher. Yunus was the last to return.
+    content: `One day, Taptuk Emre said to his students, “Today, all of you go up the mountain and bring me flowers. I will give a gift to whoever prepares the most beautiful bunch of flowers.” All the dervishes went out into the fields. They picked flowers and ran back to their teacher. Yunus was the last to return.
 
 In the late afternoon, he returned with a single daisy. Some dervishes were making fun of Yunus. They whispered in each other’s ears, “Just look at him! All he could find was a single daisy!” Taptuk asked Yunus about the meaning and the reason for this single daisy. Yunus replied, “My teacher, I walked around the fields, and wherever I saw a flower, I heard that it was saying the name of Allah. I could not cut any of them. When it started to get dark, a daisy called out to me:`,
     vocabulary: [
@@ -160,7 +160,7 @@ In the late afternoon, he returned with a single daisy. Some dervishes were maki
     id: 8, type: 'story', title: 'A Meaningful Life',
     image: 'https://firebasestorage.googleapis.com/v0/b/gen-lang-client-0373200489.firebasestorage.app/o/yunus%2Fa2%2Fimages%2Fyunus_a2_ch8-clean.png?alt=media&token=5928fe9e-cd4a-42d3-9dbe-10d6b32126b1',
     audioUrl: 'https://firebasestorage.googleapis.com/v0/b/gen-lang-client-0373200489.firebasestorage.app/o/yunus%2Fa2%2Faudio%2F07_Chapter_8_A_Meaningful_Life.mp3?alt=media&token=0b25f43f-f374-4a92-a505-68e8aa74a66c',
-    content: `‘Come, dervish Yunus. My time is up now; I’m dried up and dying. At least pick me and let my life end in the hands of a dervish.”
+    content: `‘Come, dervish Yunus. My time is up now; I’m dried up and dying. At least pick me and let my life end in the hands of a dervish.’”
 
 In these two stories, Yunus tells us to always remember Allah in our hearts and to do every job well. He says that we must always do our best when we work. Every job is important, so we should do it well and correctly for the love of Allah. Our hearts must be full of love for Him during our daily lives. This helps us live a meaningful and fruitful life.`,
     vocabulary: [
