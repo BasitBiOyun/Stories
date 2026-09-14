97f9b6d5db7cdbc1409e6b984315c57e1aaa56f7
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-09-14 13:48:47 +0300

    Export historical entity feature

---
+++ src/features/historical-entities/index.ts
@@ -0,0 +1,15 @@
+export { HistoricalEntityWord } from './HistoricalEntityWord';
+export {
+  applyHistoricalEntitiesToPage,
+  getHistoricalEntity,
+  getHistoricalEntityIdFromDefinition,
+  historicalEntities,
+  historicalEntityDefinition,
+} from './registry';
+export type {
+  HistoricalEntity,
+  HistoricalEntityCopy,
+  HistoricalEntityKind,
+  HistoricalEntityLocale,
+  HistoricalMapFocus,
+} from './types';
