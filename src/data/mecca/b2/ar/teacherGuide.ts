4cb116ab7d7a5691c08643b2bfd1ea52ea842415
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-09-22 23:13:18 +0300

    Remove obsolete Mecca B2 template types

--- src/data/mecca/b2/ar/teacherGuide.ts
+++ src/data/mecca/b2/ar/teacherGuide.ts
@@ -3,18 +3,6 @@ import type { TeacherGuideMetadata, TeacherGuideSection } from '../../../../type
 const TYMM_FOREIGN = 'https://tymm.meb.gov.tr/beceriler/yabanci-dil-alan-becerileri';
 const TYMM_VALUES = 'https://tymm.meb.gov.tr/beceriler/erdem-deger-eylem-cercevesi';
 
-type Spec = {
-  n: number;
-  title: string;
-  focus: string;
-  misconception: string;
-  evidence: string;
-  question: string;
-  action: string;
-  transfer: string;
-  language: string;
-};
-
 export const meccaB2TeacherGuideAr: TeacherGuideSection[] = [
   {
     "chapter": "الفصل 1: بلد وعصر",
