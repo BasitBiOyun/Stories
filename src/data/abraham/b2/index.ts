95001b3ed1577d6b9644a1ab57d4f422a6ef4adc
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-09-25 21:33:37 +0300

    Move guide payloads out of reader book chunk

--- src/data/abraham/b2/index.ts
+++ src/data/abraham/b2/index.ts
@@ -1,10 +1,6 @@
 import type { BookData, PageData } from '../../../types';
 import { abrahamB2Pages } from './en/pages';
 import { abrahamB2PagesAr } from './ar/pages';
-import { abrahamB2TeacherGuideEn, abrahamB2TeacherGuideMetadata } from './en/teacherGuide';
-import { abrahamB2TeacherGuideAr, abrahamB2TeacherGuideMetadataAr } from './ar/teacherGuide';
-import { abrahamB2SelfStudyGuideEn } from './en/selfStudyGuide';
-import { abrahamB2SelfStudyGuideAr } from './ar/selfStudyGuide';
 
 const ABRAHAM_B2_ENGLISH_GLOSSARY_EXCLUSIONS = new Set([
   'al-maqam',
@@ -39,9 +35,8 @@ export const abrahamB2BookDataEn: BookData = {
   level: 'B2',
   baseFontSize: 12,
   pages: buildEnglishPages(),
-  teacherGuide: abrahamB2TeacherGuideEn,
-  teacherGuideMetadata: abrahamB2TeacherGuideMetadata,
-  selfStudyGuide: abrahamB2SelfStudyGuideEn,
+  teacherGuide: [],
+  selfStudyGuide: [],
 };
 
 export const abrahamB2BookDataAr: BookData = {
@@ -50,9 +45,8 @@ export const abrahamB2BookDataAr: BookData = {
   level: 'B2',
   baseFontSize: 14,
   pages: abrahamB2PagesAr,
-  teacherGuide: abrahamB2TeacherGuideAr,
-  teacherGuideMetadata: abrahamB2TeacherGuideMetadataAr,
-  selfStudyGuide: abrahamB2SelfStudyGuideAr,
+  teacherGuide: [],
+  selfStudyGuide: [],
 };
 
 export const abrahamB2BookData = abrahamB2BookDataEn;
