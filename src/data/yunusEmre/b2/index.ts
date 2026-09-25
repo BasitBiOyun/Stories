73758938357978cd0e5a6b473e04652fa8de738d
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-09-25 21:34:22 +0300

    Move guide payloads out of reader book chunk

--- src/data/yunusEmre/b2/index.ts
+++ src/data/yunusEmre/b2/index.ts
@@ -2,10 +2,6 @@ import type { BookData, PageData } from '../../../types';
 
 import { yunusB2Pages } from './en/pages';
 import { yunusEmreB2PagesAr } from './ar/pages';
-import { yunusB2TeacherGuide, yunusB2TeacherGuideMetadata } from './en/teacherGuide';
-import { yunusB2SelfStudyGuide, yunusB2StudentGuideMetadata } from './en/selfStudyGuide';
-import { yunusEmreB2TeacherGuideAr, yunusEmreB2TeacherGuideMetadataAr } from './ar/teacherGuide';
-import { yunusEmreB2SelfStudyGuideAr, yunusEmreB2StudentGuideMetadataAr } from './ar/selfStudyGuide';
 
 const normalizeTerm = (word: string) => word.trim().toLocaleLowerCase();
 
@@ -67,10 +63,8 @@ export const yunusEmreB2BookDataEn: BookData = {
   level: 'B2',
   baseFontSize: 13,
   pages: englishPages,
-  teacherGuide: yunusB2TeacherGuide,
-  teacherGuideMetadata: yunusB2TeacherGuideMetadata,
-  selfStudyGuide: yunusB2SelfStudyGuide,
-  studentGuideMetadata: yunusB2StudentGuideMetadata,
+  teacherGuide: [],
+  selfStudyGuide: [],
 };
 
 export const yunusEmreB2BookDataAr: BookData = {
@@ -79,10 +73,8 @@ export const yunusEmreB2BookDataAr: BookData = {
   level: 'B2',
   baseFontSize: 14,
   pages: arabicPages,
-  teacherGuide: yunusEmreB2TeacherGuideAr,
-  teacherGuideMetadata: yunusEmreB2TeacherGuideMetadataAr,
-  selfStudyGuide: yunusEmreB2SelfStudyGuideAr,
-  studentGuideMetadata: yunusEmreB2StudentGuideMetadataAr,
+  teacherGuide: [],
+  selfStudyGuide: [],
 };
 
 export const yunusEmreB2BookData = yunusEmreB2BookDataEn;
